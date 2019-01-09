70212e24a146495f6cad6fbb45892f6ba1a27277
datagrinder <data@hdm.io>
2019-01-09 01:02:49 -0600

    Update 2019-01-09

---
+++ data/js/34/e1/d1/90/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("34e1d1900000/28",[{"d":"2019-01-09","t":"add","s":"ieee-mam.csv","a":"Industriepark Brechtsebaan 8 bus 1 Schoten  BE 2900","c":"BE","o":"Apart Audio NV"}]);
---
+++ data/js/90/4d/c3/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("904dc3000000/24",[{"d":"2019-01-09","t":"add","s":"ieee-oui.csv","a":"Islandsvej 29 Horsens  DK 8700","c":"DK","o":"Flonidan A/S"}]);
