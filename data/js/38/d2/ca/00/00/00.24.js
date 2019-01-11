eddb4e2f537c451684abe0af803acebd941ba926
datagrinder <data@hdm.io>
2019-01-11 01:02:16 -0600

    Update 2019-01-11

---
+++ data/js/38/d2/ca/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("38d2ca000000/24",[{"d":"2019-01-11","t":"add","s":"ieee-oui.csv","a":"Ali Center,No.3331 Keyuan South RD (Shenzhen bay), Nanshan District, Shenzhen Guangdong province Shenzhen GuangDong CN 518000","c":"CN","o":"Zhejiang Tmall Technology Co., Ltd."}]);
