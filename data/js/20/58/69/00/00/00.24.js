ed7cc73286794a881d735610325197950de39064
datagrinder <data@hdm.io>
2019-01-12 01:02:41 -0600

    Update 2019-01-12

---
+++ data/js/20/58/69/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("205869000000/24",[{"d":"2019-01-12","t":"add","s":"ieee-oui.csv","a":"350 West Java Drive Sunnyvale CA US 94089","c":"US","o":"Ruckus Wireless"}]);
---
+++ data/js/9c/49/7f/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("9c497f000000/24",[{"d":"2019-01-12","t":"add","s":"ieee-oui.csv","a":"Phase 3, Bayan Lepas FIZ Bayan Lepas Penang MY 11900","c":"MY","o":"Integrated Device Technology (Malaysia) Sdn. Bhd."}]);
