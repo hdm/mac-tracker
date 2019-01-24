da987d70ce97e5a030b38107a4dc3edc78d174a2
datagrinder <data@hdm.io>
2019-01-24 01:02:02 -0600

    Update 2019-01-24

---
+++ data/js/70/b3/d5/6b/a0/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d56ba000/36",[{"d":"2019-01-24","t":"add","s":"ieee-oui36.csv","a":"plac Zwyciestwa 2 bud. D Lodz lodzkie PL 90-312","c":"PL","o":"Integrotech sp. z o.o."}]);
---
+++ data/js/84/e8/92/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("84e892000000/24",[{"d":"2019-01-24","t":"add","s":"ieee-oui.csv","a":"301 Olcott St Santa Clara CA US 95054","c":"US","o":"Actiontec Electronics, Inc"}]);
