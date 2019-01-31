c3ec74fabdc05bf4f4c61f6ed492a435f4e9ed33
datagrinder <data@hdm.io>
2019-01-31 01:01:48 -0600

    Update 2019-01-31

---
+++ data/js/18/46/44/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("184644000000/24",[{"d":"2019-01-31","t":"add","s":"ieee-oui.csv","a":"151 Lorong Chuan Singapore  SG 556741","c":"SG","o":"Home Control Singapore Pte Ltd"}]);
---
+++ data/js/70/b3/d5/69/a0/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d569a000/36",[{"d":"2019-01-31","t":"add","s":"ieee-oui36.csv","a":"Chaussée Verte, 93B Saint-Georges  BE 4470","c":"BE","o":"Altaneos"}]);
