092d323fec3584ba537ee384b7f2ae9a7535a765
datagrinder <data@hdm.io>
2019-02-09 01:01:51 -0600

    Update 2019-02-09

---
+++ data/js/68/82/f2/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("6882f2000000/24",[{"d":"2019-02-09","t":"add","s":"ieee-oui.csv","a":"Holzmarkt 1  Cologne NW DE 50676","c":"DE","o":"grandcentrix GmbH"}]);
---
+++ data/js/8a/fb/16/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("8afb16000000/24",[{"d":"2019-02-09","t":"add","s":"ieee-cid.csv","a":"10220 Lazy Bear St Las Vegas NV US 89131","c":"US","o":"TeraByte, Inc."}]);
