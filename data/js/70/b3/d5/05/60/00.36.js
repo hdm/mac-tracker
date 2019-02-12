739f22879eecd09e4832ea8d9bc78f37e383f673
datagrinder <data@hdm.io>
2019-02-12 01:02:05 -0600

    Update 2019-02-12

---
+++ data/js/70/b3/d5/05/60/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d5056000/36",[{"d":"2019-02-12","t":"add","s":"ieee-oui36.csv","a":"GYEONGGI-DO SEONGNAM-SI JUNGWON-GU KR 13376","c":"KR","o":"MIRAE INFORMATION TECHNOLOGY CO., LTD."}]);
