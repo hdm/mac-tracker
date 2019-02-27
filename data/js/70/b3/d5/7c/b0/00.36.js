054a963aa7ae01664a8cc57874f736698d2b05c3
datagrinder <data@hdm.io>
2019-02-27 01:02:03 -0600

    Update 2019-02-27

--- data/js/70/b3/d5/7c/b0/00.36.js
+++ data/js/70/b3/d5/7c/b0/00.36.js
@@ -1,1 +1,1 @@
-deepmacDetailCallback("70b3d57cb000/36",[{"d":"2019-02-26","t":"add","s":"ieee-oui36.csv","a":"7763 Old Telegraph Road Severn  US 21037","c":"US","o":"KeyW Corporation"}]);
+deepmacDetailCallback("70b3d57cb000/36",[{"d":"2019-02-26","t":"add","s":"ieee-oui36.csv","a":"7763 Old Telegraph Road Severn  US 21037","c":"US","o":"KeyW Corporation"},{"d":"2019-02-27","t":"change","s":"ieee-oui36.csv","a":"7763 Old Telegraph Road Severn MD US 21144","c":"US","o":"KeyW Corporation"}]);
---
+++ data/js/84/8b/cd/70/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("848bcd700000/28",[{"d":"2019-02-27","t":"add","s":"ieee-mam.csv","a":"Keji North 3rd Rd Shenzhen guangdong CN 518000","c":"CN","o":"Smart Code (Shenzhen) Technology Co.,Ltd"}]);
