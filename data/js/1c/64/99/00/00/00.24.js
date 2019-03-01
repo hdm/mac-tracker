709dd24733a8641250893381fa73fdd693b543f2
datagrinder <data@hdm.io>
2019-03-01 01:03:30 -0600

    Update 2019-03-01

---
+++ data/js/1c/64/99/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("1c6499000000/24",[{"d":"2019-03-01","t":"add","s":"ieee-oui.csv","a":"3F-1, 10 Lane 609, New Taipei City , Taiwan TW 24159","c":"TW","o":"Comtrend Corporation"}]);
---
+++ data/js/98/0d/67/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("980d67000000/24",[{"d":"2019-03-01","t":"add","s":"ieee-oui.csv","a":"No. 6 Innovation Road II, Science Park Hsichu Taiwan TW 300","c":"TW","o":"Zyxel Communications Corporation"}]);
