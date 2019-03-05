0087c3b025232a3d6d539f9f6e5aabc25bd885e9
datagrinder <data@hdm.io>
2019-03-05 01:02:08 -0600

    Update 2019-03-05

---
+++ data/js/1c/82/59/40/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("1c8259400000/28",[{"d":"2019-03-05","t":"add","s":"ieee-mam.csv","a":"Beeschi Mattenstrasse 2 Steg Wallis CH 3940","c":"CH","o":"winsun AG"}]);
---
+++ data/js/74/38/b7/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("7438b7000000/24",[{"d":"2019-03-05","t":"add","s":"ieee-oui.csv","a":"30-2 Shimomaruko 3-chome Ohta-ku Tokyo JP 146-8501","c":"JP","o":"CANON INC."}]);
