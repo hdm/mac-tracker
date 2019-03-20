8505b24841272facddecda8af831fe321903a039
datagrinder <data@hdm.io>
2019-03-20 01:02:00 -0500

    Update 2019-03-20

---
+++ data/js/bc/97/40/00/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("bc9740000000/28",[{"d":"2019-03-20","t":"add","s":"ieee-mam.csv","a":"JiuHua Road 888, Nantong High-Tech Industrial Development Zone, Nantong Jiangsu CN 226300","c":"CN","o":"Alpha ESS Co., Ltd."}]);
---
+++ data/js/dc/a6/32/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("dca632000000/24",[{"d":"2019-03-20","t":"add","s":"ieee-oui.csv","a":"Maurice Wilkes Building, Cowley Road Cambridge  GB CB4 0DS","c":"GB","o":"Raspberry Pi Trading Ltd"}]);
