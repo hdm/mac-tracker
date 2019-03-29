5444cb33e65c39be2eb845e8954af0c1bf1271cb
datagrinder <data@hdm.io>
2019-03-29 01:02:03 -0500

    Update 2019-03-29

---
+++ data/js/50/97/44/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("509744000000/24",[{"d":"2019-03-29","t":"add","s":"ieee-oui.csv","a":"Phase 3, Bayan Lepas FIZ Bayan Lepas Penang MY 11900","c":"MY","o":"Integrated Device Technology (Malaysia) Sdn. Bhd."}]);
