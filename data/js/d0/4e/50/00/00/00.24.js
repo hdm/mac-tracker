7ee33e0f65dd59bf586078d74ef507350cc0e57e
datagrinder <data@hdm.io>
2019-04-17 01:02:51 -0500

    Update 2019-04-17

---
+++ data/js/d0/4e/50/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("d04e50000000/24",[{"d":"2019-04-17","t":"add","s":"ieee-oui.csv","a":"No.999 Dacheng East Road, Fenghua District, Ningbo City , Zhejiang Province, PRC Ningbo Zhejiang CN 315500","c":"CN","o":"Mobiwire Mobiles (NingBo) Co., LTD"}]);
