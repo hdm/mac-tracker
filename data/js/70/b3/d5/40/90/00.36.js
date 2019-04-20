0b35fbcff356f7ab6ed9de6258e5efe99ea1b308
datagrinder <data@hdm.io>
2019-04-20 01:02:06 -0500

    Update 2019-04-20

---
+++ data/js/70/b3/d5/40/90/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d5409000/36",[{"d":"2019-04-20","t":"add","s":"ieee-oui36.csv","a":"First floor, Jiangshan Holding Building, No.6 Academy of Agricultural Sciences  West Road, Haidian District Beijing Beijing CN 100097","c":"CN","o":"Beijing Yutian Technology Co., Ltd."}]);
---
+++ data/js/a4/53/0e/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("a4530e000000/24",[{"d":"2019-04-20","t":"add","s":"ieee-oui.csv","a":"80 West Tasman Drive San Jose CA US 94568","c":"US","o":"Cisco Systems, Inc"}]);
