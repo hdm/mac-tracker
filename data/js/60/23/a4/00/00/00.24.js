b34bffbb9c62612bffd56ff14776cae7de396d95
datagrinder <data@hdm.io>
2019-04-23 01:04:18 -0500

    Update 2019-04-23

---
+++ data/js/60/23/a4/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("6023a4000000/24",[{"d":"2019-04-23","t":"add","s":"ieee-oui.csv","a":"Anzhou,Industrial Park Anzhou,Industrial Park Sichuan CN 621000","c":"CN","o":"Sichuan AI-Link Technology Co., Ltd."}]);
---
+++ data/js/c0/cb/f1/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("c0cbf1000000/24",[{"d":"2019-04-23","t":"add","s":"ieee-oui.csv","a":"No.999 Dacheng East Road, Fenghua District, Ningbo City , Zhejiang Province, PRC Ningbo Zhejiang CN 315500","c":"CN","o":"Mobiwire Mobiles (NingBo) Co., LTD"}]);
