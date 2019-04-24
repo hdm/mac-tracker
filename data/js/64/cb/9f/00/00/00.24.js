cb3c2211ab254a0a13e5498ec208c5e30a103d6a
datagrinder <data@hdm.io>
2019-04-24 01:01:55 -0500

    Update 2019-04-24

---
+++ data/js/64/cb/9f/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("64cb9f000000/24",[{"d":"2019-04-24","t":"add","s":"ieee-oui.csv","a":"ROOMS 05-15, 13A/F., SOUTH TOWER, WORLD FINANCE CENTRE, HARBOUR CITY, 17 CANTON ROAD, TSIM SHA TSUI, KOWLOON, HONG KONG Hong Kong Hong Kong HK 999077","c":"HK","o":"TECNO MOBILE LIMITED"}]);
---
+++ data/js/70/b3/d5/1f/c0/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d51fc000/36",[{"d":"2019-04-24","t":"add","s":"ieee-oui36.csv","a":"No.127, Jianguo 1st Rd., Lingya Dist.  Kaohsiung City   TW 802","c":"TW","o":"Guan Show Technologe Co., Ltd."}]);
