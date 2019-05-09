f5142501ac08ce898b28c20be1092a74a244e07e
datagrinder <data@hdm.io>
2019-05-09 01:02:02 -0500

    Update 2019-05-09

---
+++ data/js/d0/5f/64/10/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("d05f64100000/28",[{"d":"2019-05-09","t":"add","s":"ieee-mam.csv","a":"4F, Block A, B, Building 2, NO.321, Jinpeng Street, Xihu District Hangzhou Zhejiang CN 310030","c":"CN","o":"Hangzhou ToupTek Photonics Co., Ltd."}]);
