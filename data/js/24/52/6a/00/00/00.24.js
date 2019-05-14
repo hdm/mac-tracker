5d2e72d9945dfeca059e9d404e66d7b5a74bae38
datagrinder <data@hdm.io>
2019-05-14 01:02:55 -0500

    Update 2019-05-14

---
+++ data/js/24/52/6a/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("24526a000000/24",[{"d":"2019-05-14","t":"add","s":"ieee-oui.csv","a":"No.1199,Waterfront Road  Hangzhou Zhejiang CN 310053","c":"CN","o":"Zhejiang Dahua Technology Co., Ltd."}]);
---
+++ data/js/d4/6b/a6/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("d46ba6000000/24",[{"d":"2019-05-14","t":"add","s":"ieee-oui.csv","a":"No.2 Xin Cheng Road, Room R6,Songshan Lake Technology Park Dongguan  CN 523808","c":"CN","o":"HUAWEI TECHNOLOGIES CO.,LTD"}]);
