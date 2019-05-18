11dc4bd757fe2aa7436f6264d965460d643c1519
datagrinder <data@hdm.io>
2019-05-18 01:02:23 -0500

    Update 2019-05-18

---
+++ data/js/70/b3/d5/7f/f0/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d57ff000/36",[{"d":"2019-05-18","t":"add","s":"ieee-oui36.csv","a":"Gewerbeparkstrasse 9 Anif Salzburg AT 5081","c":"AT","o":"eumig industrie-TV GmbH."}]);
---
+++ data/js/70/b3/d5/d3/10/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d5d31000/36",[{"d":"2019-05-18","t":"add","s":"ieee-oui36.csv","a":"535 Legget Drive, Kanata Ontario CA K2K3B8","c":"CA","o":"Solace Systems Inc."}]);
