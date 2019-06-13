af06feed13297073ec150759169ed6a499d805ac
datagrinder <data@hdm.io>
2019-06-13 01:02:19 -0500

    Update 2019-06-13

---
+++ data/js/20/85/93/d0/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("208593d00000/28",[{"d":"2019-06-13","t":"add","s":"ieee-mam.csv","a":"15/F,NO3003,Baoyang Road,Baoshan District Shanghai Shanghai CN 201201","c":"CN","o":"Shanghai Kenmyond Industrial Network Equipment Co.,Ltd"}]);
---
+++ data/js/4c/f1/9e/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("4cf19e000000/24",[{"d":"2019-06-13","t":"add","s":"ieee-oui.csv","a":"Ideal Boilers, National Ave Hull East Yorkshire GB HU5 4JB","c":"GB","o":"Groupe Atlantic"}]);
