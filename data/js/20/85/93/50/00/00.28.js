70c5147d2cc2e6bc711cce9f403cb0257b918fd1
datagrinder <data@hdm.io>
2019-06-14 01:02:28 -0500

    Update 2019-06-14

---
+++ data/js/20/85/93/50/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("208593500000/28",[{"d":"2019-06-14","t":"add","s":"ieee-mam.csv","a":"8F, No.322, Sec. 1, Neihu Rd., Neihu Dist. Taipei City  TW 114","c":"TW","o":"Wave-In Communication"}]);
