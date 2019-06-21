c9968f8ef58477e6d38dca7d3492be090c0ba289
datagrinder <data@hdm.io>
2019-06-21 01:02:18 -0500

    Update 2019-06-21

---
+++ data/js/58/85/e9/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("5885e9000000/24",[{"d":"2019-06-21","t":"add","s":"ieee-oui.csv","a":"No.24 Nichang Boulevard, Huixing Block, Yubei District, Chongqing. Chongqing China CN 401120","c":"CN","o":"Realme Chongqing MobileTelecommunications Corp Ltd"}]);
