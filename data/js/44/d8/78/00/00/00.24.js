f77a2524dca91fb9fc78767dd7fd7bed35038a80
datagrinder <data@hdm.io>
2019-06-28 01:02:48 -0500

    Update 2019-06-28

---
+++ data/js/44/d8/78/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("44d878000000/24",[{"d":"2019-06-28","t":"add","s":"ieee-oui.csv","a":"No.75,Zhongkai High-Tech Development District,Huizhou Hui Zhou Guangdong CN 516006","c":"CN","o":"Hui Zhou Gaoshengda Technology Co.,LTD"}]);
---
+++ data/js/70/b3/d5/bf/c0/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d5bfc000/36",[{"d":"2019-06-28","t":"add","s":"ieee-oui36.csv","a":"Box 423 Karlskoga  SE SE-691 27","c":"SE","o":"Vishay Nobel AB"}]);
