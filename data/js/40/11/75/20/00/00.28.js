cfbba209867238364d24d07b4d4a99b8c979cae6
datagrinder <data@hdm.io>
2019-07-05 01:02:41 -0500

    Update 2019-07-05

---
+++ data/js/40/11/75/20/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("401175200000/28",[{"d":"2019-07-05","t":"add","s":"ieee-mam.csv","a":"3-6 Kandacho HImeji Hyogo JP 6700991","c":"JP","o":"Kanda Kogyo"}]);
---
+++ data/js/ec/be/dd/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("ecbedd000000/24",[{"d":"2019-07-05","t":"add","s":"ieee-oui.csv","a":"250, route de l'Empereur Rueil Malmaison Cedex hauts de seine FR 92848","c":"FR","o":"Sagemcom Broadband SAS"}]);
