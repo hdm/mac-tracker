69a677fdc6884bfbb91fa024924cdea6e4b6664d
datagrinder <data@hdm.io>
2019-07-23 01:02:22 -0500

    Update 2019-07-23

---
+++ data/js/b4/02/16/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("b40216000000/24",[{"d":"2019-07-23","t":"add","s":"ieee-oui.csv","a":"80 West Tasman Drive San Jose CA US 94568","c":"US","o":"Cisco Systems, Inc"}]);
---
+++ data/js/f8/9e/28/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("f89e28000000/24",[{"d":"2019-07-23","t":"add","s":"ieee-oui.csv","a":"500 Terry A. Francois Blvd San Francisco  US 94158","c":"US","o":"Cisco Meraki"}]);
