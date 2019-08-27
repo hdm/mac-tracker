15a87eb786b1d96866c7c7626bb2340eb73b40df
datagrinder <data@hdm.io>
2019-08-27 01:03:05 -0500

    Update 2019-08-27

---
+++ data/js/50/62/55/40/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("506255400000/28",[{"d":"2019-08-27","t":"add","s":"ieee-mam.csv","a":"#1103, 11, DIGITAL-RO 33-GIL, GURO-GU SEOUL  KR 08380","c":"KR","o":"XSLAB Inc."}]);
---
+++ data/js/70/b3/d5/5b/40/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d55b4000/36",[{"d":"2019-08-27","t":"add","s":"ieee-oui36.csv","a":"11310 N Government Way Hayden ID US 83835","c":"US","o":"Systems Technologies"}]);
