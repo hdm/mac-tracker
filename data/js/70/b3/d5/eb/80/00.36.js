e52c338fc825c59fb221a3dd6e7836e03d00cf6b
datagrinder <data@hdm.io>
2019-09-06 01:01:44 -0500

    Update 2019-09-06

---
+++ data/js/70/b3/d5/eb/80/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d5eb8000/36",[{"d":"2019-09-06","t":"add","s":"ieee-oui36.csv","a":"7901 Shaffer Pkwy Littleton CO US 80127","c":"US","o":"Emporia Renewable Energy Corp"}]);
---
+++ data/js/ac/1e/92/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("ac1e92000000/24",[{"d":"2019-09-06","t":"add","s":"ieee-oui.csv","a":"129, Samsung-ro, Youngtongl-Gu Suwon  KR 16677","c":"KR","o":"Samsung Electronics Co.,LTD"}]);
