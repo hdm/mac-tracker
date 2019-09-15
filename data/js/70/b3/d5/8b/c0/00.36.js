729fa45dd6a0b12be45917af510d186a164740bc
datagrinder <data@hdm.io>
2019-09-15 01:02:41 -0500

    Update 2019-09-15

---
+++ data/js/70/b3/d5/8b/c0/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d58bc000/36",[{"d":"2019-09-15","t":"add","s":"ieee-oui36.csv","a":"Ahornweg 5A Othmarsingen  CH 5504","c":"CH","o":"GSI GeoSolutions International Ltd"}]);
