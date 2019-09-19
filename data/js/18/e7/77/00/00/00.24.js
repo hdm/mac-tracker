f6049944d7e57f2a3cb2f17bafa8b507f57ed8e0
datagrinder <data@hdm.io>
2019-09-19 01:02:15 -0500

    Update 2019-09-19

---
+++ data/js/18/e7/77/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("18e777000000/24",[{"d":"2019-09-19","t":"add","s":"ieee-oui.csv","a":"#283,BBK Road Wusha,Chang'An DongGuan City,Guangdong, CN 523860","c":"CN","o":"vivo Mobile Communication Co., Ltd."}]);
