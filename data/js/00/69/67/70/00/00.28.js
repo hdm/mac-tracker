bc6005f15f386935f688bc6759fadcdd058941fb
datagrinder <data@hdm.io>
2019-09-22 01:02:42 -0500

    Update 2019-09-22

---
+++ data/js/00/69/67/70/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("006967700000/28",[{"d":"2019-09-22","t":"add","s":"ieee-mam.csv","a":"774, Yuseong-daero, Yuseong-gu,  Daejeon Daejeon KR 34171","c":"KR","o":"PANGAEA SOLUTION INC"}]);
---
+++ data/js/70/b3/d5/ce/f0/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d5cef000/36",[{"d":"2019-09-22","t":"add","s":"ieee-oui36.csv","a":"Linnapellontie 18 Halikko  FI 24910","c":"FI","o":"Ellego Powertec Oy"}]);
