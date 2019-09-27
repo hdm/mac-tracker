386e45ea107fafab41a848da85f1fe15dfcab60b
datagrinder <data@hdm.io>
2019-09-27 01:03:11 -0500

    Update 2019-09-27

---
+++ data/js/98/df/82/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("98df82000000/24",[{"d":"2019-09-27","t":"add","s":"ieee-oui.csv","a":"No.555 Qianmo Road, Binjiang District Hangzhou Zhejiang CN 310052","c":"CN","o":"Hangzhou Hikvision Digital Technology Co.,Ltd."}]);
---
+++ data/js/ea/11/5d/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("ea115d000000/24",[{"d":"2019-09-27","t":"add","s":"ieee-cid.csv","a":"NO. 2-3, INDUSTRY EAST ROAD 2, SCIENCE PARK, HSINCHU  TW 30075","c":"TW","o":"AUTOVISION TECHNOLOGY INC."}]);
