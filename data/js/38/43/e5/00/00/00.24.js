2e475fd678f382505610942892f40ccd8a1b658e
datagrinder <data@hdm.io>
2019-10-05 01:02:09 -0500

    Update 2019-10-05

---
+++ data/js/38/43/e5/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("3843e5000000/24",[{"d":"2019-10-05","t":"add","s":"ieee-oui.csv","a":"19, Ojeongongeop-gil Uiwang-si Gyeonggi-do KR 16072","c":"KR","o":"Grotech Inc"}]);
---
+++ data/js/a4/30/7a/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("a4307a000000/24",[{"d":"2019-10-05","t":"add","s":"ieee-oui.csv","a":"129, Samsung-ro, Youngtongl-Gu Suwon Gyeonggi-Do KR 16677","c":"KR","o":"Samsung Electronics Co.,Ltd"}]);
