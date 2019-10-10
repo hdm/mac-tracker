5081eea44e698330e1f0e4fd3aea3b9d92dc3be1
datagrinder <data@hdm.io>
2019-10-10 01:02:30 -0500

    Update 2019-10-10

---
+++ data/js/34/f1/50/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("34f150000000/24",[{"d":"2019-10-10","t":"add","s":"ieee-oui.csv","a":"No.75,Zhongkai High-Tech Development District,Huizhou Hui Zhou Guangdong CN 516006","c":"CN","o":"Hui Zhou Gaoshengda Technology Co.,LTD"}]);
---
+++ data/js/f4/d9/c6/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("f4d9c6000000/24",[{"d":"2019-10-10","t":"add","s":"ieee-oui.csv","a":"No.5,Huitai Road,Huinan High-Tech Park,Huiao Highway Huizhou Guangdong CN 516025","c":"CN","o":"UNIONMAN TECHNOLOGY CO.,LTD"}]);
