41567d03d0ef4e4cc4a95c84432ac452058ff90d
datagrinder <data@hdm.io>
2019-10-19 01:02:29 -0500

    Update 2019-10-19

--- data/js/70/b3/d5/3f/10/00.36.js
+++ data/js/70/b3/d5/3f/10/00.36.js
@@ -1,1 +1,1 @@
-deepmacDetailCallback("70b3d53f1000/36",[{"a":"505 Boul Du Parc Technologique Quebec QUEBEC CA G1P 4S9","o":"Olympus NDT Canada","d":"2014-02-09","t":"add","s":"ieee","c":"CA"}]);
+deepmacDetailCallback("70b3d53f1000/36",[{"a":"505 Boul Du Parc Technologique Quebec QUEBEC CA G1P 4S9","o":"Olympus NDT Canada","d":"2014-02-09","t":"add","s":"ieee","c":"CA"},{"d":"2019-10-19","t":"change","s":"ieee-oui36.csv","a":"3415 rue Pierre Ardouin Québec Québec CA G1P 0B3","c":"CA","o":"Olympus NDT Canada"}]);
