9fadc966f7d3f74eb77d830dbf256fc5921926df
datagrinder <data@hdm.io>
2019-10-24 01:02:04 -0500

    Update 2019-10-24

---
+++ data/js/6c/c6/3b/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("6cc63b000000/24",[{"d":"2019-10-24","t":"add","s":"ieee-oui.csv","a":"89# Jiang Nan RD Suzhou Jiangsu CN 215412","c":"CN","o":"Taicang T&W Electronics"}]);
---
+++ data/js/70/b3/d5/ca/f0/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d5caf000/36",[{"d":"2019-10-24","t":"add","s":"ieee-oui36.csv","a":"VIA TALPONEDO 29/A PORCIA PORDENONE IT 330850","c":"IT","o":"DAVE SRL"}]);
