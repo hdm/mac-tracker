c17173266f731f0a42f35591a0f145dbbb9d099c
datagrinder <data@hdm.io>
2019-11-24 01:02:31 -0600

    Update 2019-11-24

---
+++ data/js/10/52/1c/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("10521c000000/24",[{"d":"2019-11-24","t":"add","s":"ieee-oui.csv","a":"Room 204, Building 2, 690 Bibo Rd, Pudong New Area Shanghai Shanghai CN 201203","c":"CN","o":"Espressif Inc."}]);
---
+++ data/js/b0/b3/53/30/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("b0b353300000/28",[{"d":"2019-11-24","t":"add","s":"ieee-mam.csv","a":"C/ Crisol 3 RIVAS-VACIAMADRID Madrid ES 28522","c":"ES","o":"AD HOC DEVELOPMENTS S.L"}]);
