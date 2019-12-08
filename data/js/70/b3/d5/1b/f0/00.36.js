3d653afd42db763c48b5ca6d9a6e3889da69d249
datagrinder <data@hdm.io>
2019-12-08 01:02:50 -0600

    Update 2019-12-08

---
+++ data/js/70/b3/d5/1b/f0/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d51bf000/36",[{"d":"2019-12-08","t":"add","s":"ieee-oui36.csv","a":"Paffrather Str. 140 Bergisch Gladbach North Rhine-Westphalia DE 51465","c":"DE","o":"DEUTA-WERKE GmbH"}]);
---
+++ data/js/70/b3/d5/7d/b0/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d57db000/36",[{"d":"2019-12-08","t":"add","s":"ieee-oui36.csv","a":"Arnold-Sommerfeld-Ring 2 Baesweiler NRW DE 52499","c":"DE","o":"aquila biolabs GmbH"}]);
