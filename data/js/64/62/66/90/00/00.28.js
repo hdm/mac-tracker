f3ff2f166fd070d67a028b124e96f6d4530458c0
datagrinder <data@hdm.io>
2019-12-19 01:02:21 -0600

    Update 2019-12-19

---
+++ data/js/64/62/66/90/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("646266900000/28",[{"d":"2019-12-19","t":"add","s":"ieee-mam.csv","a":"2F., No. 35, Aiguo E. Rd., Taipei  TW 106","c":"TW","o":"Chunghwa System Integration Co., Ltd."}]);
---
+++ data/js/70/b3/d5/5a/f0/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d55af000/36",[{"d":"2019-12-19","t":"add","s":"ieee-oui36.csv","a":"Steenbokstraat 33 APELDOORN Gelderland NL 7324 AZ","c":"AZ","o":"JENG IoT BV"}]);
