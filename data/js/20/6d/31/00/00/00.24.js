169a750b6e20aea9e0b3fb88a55a7d5fc6745eae
datagrinder <data@hdm.io>
2019-12-26 01:02:27 -0600

    Update 2019-12-26

---
+++ data/js/20/6d/31/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("206d31000000/24",[{"d":"2019-12-26","t":"add","s":"ieee-oui.csv","a":"75 E. Santa Clara St. STE 600 San Jose CA US 95113","c":"US","o":"FIREWALLA INC"}]);
---
+++ data/js/20/9e/f7/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("209ef7000000/24",[{"d":"2019-12-26","t":"add","s":"ieee-oui.csv","a":"6480 Via Del Oro San Jose CA US 95119","c":"US","o":"Extreme Networks, Inc."}]);
