772a4d393dd0d45553317dd75ccef2b6078e4683
datagrinder <data@hdm.io>
2020-01-04 01:02:20 -0600

    Update 2020-01-04

---
+++ data/js/bc/ff/21/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("bcff21000000/24",[{"d":"2020-01-04","t":"add","s":"ieee-oui.csv","a":"Room 1206, Satellite Building,2002 Keyuan Road, Nanshan  Shenzhen  Guangdong (Province)  CN 518000","c":"CN","o":"Smart Code(shenzhen)Technology Co.,Ltd"}]);
