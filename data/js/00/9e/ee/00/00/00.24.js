8a028a204a74a92ad9d608745b3c1bf29d10b830
datagrinder <data@hdm.io>
2020-01-28 01:02:20 -0600

    Update 2020-01-28

---
+++ data/js/00/9e/ee/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("009eee000000/24",[{"d":"2020-01-28","t":"add","s":"ieee-oui.csv","a":"João Bettega, 5200 Curitiba Paraná BR 81350-000","c":"BR","o":"Positivo Tecnologia S.A."}]);
--- data/js/40/47/6a/00/00/00.24.js
+++ data/js/40/47/6a/00/00/00.24.js
@@ -1,1 +1,1 @@
-deepmacDetailCallback("40476a000000/24",[{"d":"2015-12-15","t":"add","a":"655 4th St. San Francisco CA US 94107","c":"US","o":"AG Acquisition Corp. d.b.a. ASTRO Gaming"}]);
+deepmacDetailCallback("40476a000000/24",[{"d":"2015-12-15","t":"add","a":"655 4th St. San Francisco CA US 94107","c":"US","o":"AG Acquisition Corp. d.b.a. ASTRO Gaming"},{"d":"2020-01-28","t":"change","s":"ieee-oui.csv","a":"340 Bryant St., Suite 101 San Francisco CA US 94107","c":"US","o":"Astro Gaming"}]);
