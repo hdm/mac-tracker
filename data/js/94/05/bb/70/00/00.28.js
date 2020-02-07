7aa2501b11a5ad0fa1d7d52a3a6f88029d947c86
datagrinder <data@hdm.io>
2020-02-07 01:02:46 -0600

    Update 2020-02-07

--- data/js/94/05/bb/70/00/00.28.js
+++ data/js/94/05/bb/70/00/00.28.js
@@ -1,1 +1,1 @@
-deepmacDetailCallback("9405bb700000/28",[{"d":"2020-01-11","t":"add","s":"ieee-mam.csv","a":"2-2-20 Higashi-Shinagawa Shinagawa Tokyo JP 1400002","c":"JP","o":"LTE-X, Inc"}]);
+deepmacDetailCallback("9405bb700000/28",[{"d":"2020-01-11","t":"add","s":"ieee-mam.csv","a":"2-2-20 Higashi-Shinagawa Shinagawa Tokyo JP 1400002","c":"JP","o":"LTE-X, Inc"},{"d":"2020-02-07","t":"change","s":"ieee-mam.csv","a":"4F Ginza Showa Dori Building 8-14-14 Ginza Chuo-ku Tokyo JP 104-0062","c":"JP","o":"LTE-X, Inc"}]);
