a45b7baa0a1c69a53fd63d0274764f395dab952e
datagrinder <data@hdm.io>
2020-02-22 01:02:35 -0600

    Update 2020-02-22

---
+++ data/js/58/a8/7b/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("58a87b000000/24",[{"d":"2020-02-22","t":"add","s":"ieee-oui.csv","a":"199 Fremont Street, 14th Fl San Francisco CA US 94105","c":"US","o":"Fitbit, Inc."}]);
---
+++ data/js/70/b3/d5/cb/b0/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d5cbb000/36",[{"d":"2020-02-22","t":"add","s":"ieee-oui36.csv","a":"3197 Duncan Lane San Luis Obispo CA US 93401","c":"US","o":"Postmark Incorporated"}]);
