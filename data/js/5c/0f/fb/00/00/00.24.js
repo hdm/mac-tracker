6f67de0f9733869189242f9bc287a40754547d12
datagrinder <data@hdm.io>
2020-02-29 01:02:38 -0600

    Update 2020-02-29

---
+++ data/js/5c/0f/fb/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("5c0ffb000000/24",[{"d":"2020-02-29","t":"add","s":"ieee-oui.csv","a":"1010 Cambourne Business Park Cambourne Cambs GB CB23 6DP","c":"GB","o":"Amino Communications Ltd"}]);
---
+++ data/js/94/43/4d/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("94434d000000/24",[{"d":"2020-02-29","t":"add","s":"ieee-oui.csv","a":"7035 Ridge Road Hanover MD US 21076","c":"US","o":"Ciena Corporation"}]);
