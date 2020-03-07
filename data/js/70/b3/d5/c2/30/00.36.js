ebf1fdb64d4162c11c7edc2ca958bf88659840d8
datagrinder <data@hdm.io>
2020-03-07 01:02:14 -0600

    Update 2020-03-07

---
+++ data/js/70/b3/d5/c2/30/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d5c23000/36",[{"d":"2020-03-07","t":"add","s":"ieee-oui36.csv","a":"5-2, Soubiraki-cho Niihama Ehime JP 792-8588","c":"JP","o":"Sumitomo Heavy Industries, Ltd."}]);
