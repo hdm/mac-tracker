72a9924473759dcb721d20450d18db2989302712
datagrinder <data@hdm.io>
2020-03-14 01:02:24 -0500

    Update 2020-03-14

---
+++ data/js/94/fb/a7/e0/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("94fba7e00000/28",[{"d":"2020-03-14","t":"add","s":"ieee-mam.csv","a":"6F GDC Building, 9Gaoxin Centeral Avenue 3rd, Nanshan District Shenzhen Guangdong CN 518057","c":"CN","o":"Skyring Smart Technologies(Shenzhen) Co., Ltd."}]);
---
+++ data/js/e0/62/34/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("e06234000000/24",[{"d":"2020-03-14","t":"add","s":"ieee-oui.csv","a":"12500 TI Blvd Dallas TX US 75243","c":"US","o":"Texas Instruments"}]);
