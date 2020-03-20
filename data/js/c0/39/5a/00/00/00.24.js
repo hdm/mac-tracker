8fe3cc702664b51ebeaeea24e839f1468a00a66d
datagrinder <data@hdm.io>
2020-03-20 01:02:11 -0500

    Update 2020-03-20

---
+++ data/js/c0/39/5a/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("c0395a000000/24",[{"d":"2020-03-20","t":"add","s":"ieee-oui.csv","a":"No.1199,Waterfront Road  Hangzhou Zhejiang CN 310053","c":"CN","o":"Zhejiang Dahua Technology Co., Ltd."}]);
