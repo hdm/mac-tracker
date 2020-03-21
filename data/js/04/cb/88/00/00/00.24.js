ac4a5f348129adcf9409c80ad8d8972b7dcc06d7
datagrinder <data@hdm.io>
2020-03-21 01:02:09 -0500

    Update 2020-03-21

---
+++ data/js/04/cb/88/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("04cb88000000/24",[{"d":"2020-03-21","t":"add","s":"ieee-oui.csv","a":"3/F,A5 Building Zhiyuan Community No.1001,Xueyuan Road Nanshan District Shenzhen Guangdong CN 518055","c":"CN","o":"Shenzhen Jingxun Software Telecommunication Technology Co.,Ltd"}]);
