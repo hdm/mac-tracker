69e3e96003cdc547e90ab7921d375927144a11f1
datagrinder <data@hdm.io>
2020-03-27 01:02:09 -0500

    Update 2020-03-27

---
+++ data/js/70/69/79/20/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("706979200000/28",[{"d":"2020-03-27","t":"add","s":"ieee-mam.csv","a":"11-19 Wine Street Bristol  GB BS1 2PH","c":"GB","o":"Graphcore Ltd"}]);
---
+++ data/js/70/69/79/50/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("706979500000/28",[{"d":"2020-03-27","t":"add","s":"ieee-mam.csv","a":"Rua Cleia, 460 Fortaleza Ceara BR 60863280","c":"BR","o":"Ibyte"}]);
