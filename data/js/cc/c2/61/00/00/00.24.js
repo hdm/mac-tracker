dfad46c294f43af73ca5ddcc787bb167517abeb0
datagrinder <data@hdm.io>
2020-04-03 01:02:23 -0500

    Update 2020-04-03

---
+++ data/js/cc/c2/61/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("ccc261000000/24",[{"d":"2020-04-03","t":"add","s":"ieee-oui.csv","a":"445 Hoes Lane Piscataway NJ US 08554","c":"US","o":"IEEE Registration Authority"}]);
---
+++ data/js/cc/c2/61/40/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("ccc261400000/28",[{"d":"2020-04-03","t":"add","s":"ieee-mam.csv","a":"Dr.- Ludwig-Kraus-Str. 2 Gaimersheim Bavaria DE 85080","c":"DE","o":"EDAG Engineering GmbH"}]);
