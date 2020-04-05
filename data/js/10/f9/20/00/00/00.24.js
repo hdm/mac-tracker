1cfa88aab35ab814b6acf57f606b449c0f994222
datagrinder <data@hdm.io>
2020-04-05 01:02:18 -0500

    Update 2020-04-05

---
+++ data/js/10/f9/20/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("10f920000000/24",[{"d":"2020-04-05","t":"add","s":"ieee-oui.csv","a":"80 West Tasman Drive San Jose CA US 94568","c":"US","o":"Cisco Systems, Inc"}]);
---
+++ data/js/70/b3/d5/1b/20/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d51b2000/36",[{"d":"2020-04-05","t":"add","s":"ieee-oui36.csv","a":"Via Statale 11/13 Ponte S.Marco di Calcinato (BS)  IT 25011","c":"IT","o":"Cavagna Group Spa"}]);
