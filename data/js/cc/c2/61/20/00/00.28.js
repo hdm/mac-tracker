89b7ad627b6007e6e00005b38045833353bf1a8d
datagrinder <data@hdm.io>
2020-04-09 01:02:26 -0500

    Update 2020-04-09

---
+++ data/js/cc/c2/61/20/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("ccc261200000/28",[{"d":"2020-04-09","t":"add","s":"ieee-mam.csv","a":"Via Lea Cazzuoli nr.43 Mirandola Modena IT 41037","c":"IT","o":"Tecnoideal Srl"}]);
---
+++ data/js/cc/c2/61/b0/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("ccc261b00000/28",[{"d":"2020-04-09","t":"add","s":"ieee-mam.csv","a":"Schützenstrasse 3 Winterthur  CH 8401","c":"CH","o":"Winterthur Gas & Diesel Ltd."}]);
