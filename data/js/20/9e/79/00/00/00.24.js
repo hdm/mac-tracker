29b4deced24078b198ab35f6d221ae15f321d878
datagrinder <data@hdm.io>
2020-04-16 01:02:15 -0500

    Update 2020-04-16

---
+++ data/js/20/9e/79/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("209e79000000/24",[{"d":"2020-04-16","t":"add","s":"ieee-oui.csv","a":"201 E. Sandpointe Ave Santa Ana CA US 92707","c":"US","o":"Universal Electronics, Inc."}]);
---
+++ data/js/70/b3/d5/65/70/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d5657000/36",[{"d":"2020-04-16","t":"add","s":"ieee-oui36.csv","a":"3 chemin de la marbrerie Carouge  CH 1227","c":"CH","o":"ID Quantique SA"}]);
