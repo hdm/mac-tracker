d16000820342308302637f8b5d4790827c0e7eb3
datagrinder <data@hdm.io>
2020-04-29 01:01:22 +0000

    Update 2020-04-29

---
+++ data/js/70/b3/d5/17/c0/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d517c000/36",[{"d":"2020-04-29","t":"add","s":"ieee-oui36.csv","a":"806, 28 Digitalro 30gil, Gurogu Seoul  KR 08389","c":"KR","o":"Farmpro Ltd"}]);
---
+++ data/js/70/b3/d5/70/c0/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d570c000/36",[{"d":"2020-04-29","t":"add","s":"ieee-oui36.csv","a":"1609 Park 370 Place Hazelwood MO US 63042","c":"US","o":"Potter Electric Signal Co. LLC"}]);
