a4c6d489b9beb3315e203deeb5a772a984f1e9ed
datagrinder <data@hdm.io>
2020-05-24 01:02:01 +0000

    Update 2020-05-24

---
+++ data/js/34/6d/9c/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("346d9c000000/24",[{"d":"2020-05-24","t":"add","s":"ieee-oui.csv","a":"6304 Thompson Rd East Syracuse NY US 13057","c":"US","o":"Carrier Corporation"}]);
---
+++ data/js/c0/cc/42/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("c0cc42000000/24",[{"d":"2020-05-24","t":"add","s":"ieee-oui.csv","a":"No.198,First Section,Snow Mountain Avenue, Jinyuan Town, Dayi County Chengdu Sichuan CN 611330","c":"CN","o":"Sichuan Tianyi Comheart Telecom Co., Ltd."}]);
