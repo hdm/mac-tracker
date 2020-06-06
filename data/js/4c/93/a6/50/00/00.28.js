547740dd2d6b04f11642448947a3974122cf64d5
datagrinder <data@hdm.io>
2020-06-06 01:01:33 +0000

    Update 2020-06-06

---
+++ data/js/4c/93/a6/50/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("4c93a6500000/28",[{"d":"2020-06-06","t":"add","s":"ieee-mam.csv","a":"","c":"","o":"Private"}]);
---
+++ data/js/b0/30/c8/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("b030c8000000/24",[{"d":"2020-06-06","t":"add","s":"ieee-oui.csv","a":"5200 South Highland Drive Holladay  UT US 84117","c":"US","o":"Teal Drones, Inc."}]);
