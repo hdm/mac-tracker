189fc9e6ee23f6ce9f71096adf5f525a2f6b1f81
datagrinder <data@hdm.io>
2020-06-14 01:01:22 +0000

    Update 2020-06-14

---
+++ data/js/70/2c/09/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("702c09000000/24",[{"d":"2020-06-14","t":"add","s":"ieee-oui.csv","a":"11-1 HOKOTATE-CHO KAMITOBA,MINAMI-KU KYOTO KYOTO JP 601-8501","c":"JP","o":"Nintendo Co.,Ltd"}]);
