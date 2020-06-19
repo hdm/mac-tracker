e5164f617719474b9bf573cf30a14850a831a051
datagrinder <data@hdm.io>
2020-06-19 01:01:26 +0000

    Update 2020-06-19

---
+++ data/js/4c/ae/ec/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("4caeec000000/24",[{"d":"2020-06-19","t":"add","s":"ieee-oui.csv","a":"No.2, Yichuang Street, Zhongxin Guangzhou Knowledge City, Huangpu District, Guangzhou Guangzhou Guangdong CN 510555","c":"CN","o":"Guangzhou limee technology co.,LTD"}]);
---
+++ data/js/70/b3/d5/3b/90/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d53b9000/36",[{"d":"2020-06-19","t":"add","s":"ieee-oui36.csv","a":"Unit 1, 8 Theobald St THORNBURY VIC AU 3071","c":"AU","o":"BirdDog Australia"}]);
