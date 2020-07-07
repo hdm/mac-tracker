399b20ae140a0ec926a2e1442f2e653ba3bb2b44
datagrinder <data@hdm.io>
2020-07-07 01:01:26 +0000

    Update 2020-07-07

---
+++ data/js/98/fc/84/30/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("98fc84300000/28",[{"d":"2020-07-07","t":"add","s":"ieee-mam.csv","a":"Room 205E Building 5#,545 Hulan Road,Baoshan District,Shanghai City Shanghai  CN 200000","c":"CN","o":"Shanghai ZeXin Information Technologies Co. Ltd."}]);
---
+++ data/js/c0/61/9a/b0/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("c0619ab00000/28",[{"d":"2020-07-07","t":"add","s":"ieee-mam.csv","a":"De Paal 35 Almere  NL 1351JG","c":"NL","o":"Victron Energy B.V."}]);
