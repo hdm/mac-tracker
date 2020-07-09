59ef424c39c8f279636cbe858ad4ef28aeae0b06
datagrinder <data@hdm.io>
2020-07-09 01:02:36 +0000

    Update 2020-07-09

---
+++ data/js/a0/85/fc/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("a085fc000000/24",[{"d":"2020-07-09","t":"add","s":"ieee-oui.csv","a":"One Microsoft Way REDMOND WA US 98052","c":"US","o":"Microsoft Corporation"}]);
---
+++ data/js/e4/84/2b/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("e4842b000000/24",[{"d":"2020-07-09","t":"add","s":"ieee-oui.csv","a":"101 BINKANG ROAD, BINJIANG DISTRICT HANGZHOU ZHEJIANG CN 310052","c":"CN","o":"HANGZHOU SOFTEL OPTIC CO., LTD"}]);
