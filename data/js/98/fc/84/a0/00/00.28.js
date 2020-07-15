9787f29ba9bf25371e114a8608d192f71a8dd5c8
datagrinder <data@hdm.io>
2020-07-15 01:01:26 +0000

    Update 2020-07-15

---
+++ data/js/98/fc/84/a0/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("98fc84a00000/28",[{"d":"2020-07-15","t":"add","s":"ieee-mam.csv","a":"8F-5, No.57, Sec.1, Dunhua S. Rd., Songshan Dist., Taipei City  TW 10557","c":"TW","o":"Shield Inc."}]);
