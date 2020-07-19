7ca45c6be5efa5664729a50e7bc2c981f1399902
datagrinder <data@hdm.io>
2020-07-19 01:01:27 +0000

    Update 2020-07-19

---
+++ data/js/18/fd/cb/20/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("18fdcb200000/28",[{"d":"2020-07-19","t":"add","s":"ieee-mam.csv","a":"Hohstrass 1 Kloten Zuerich CH 8302","c":"CH","o":"Cabtronix AG"}]);
---
+++ data/js/18/fd/cb/60/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("18fdcb600000/28",[{"d":"2020-07-19","t":"add","s":"ieee-mam.csv","a":"Jodrell Bank, Lower Withington Macclesfield Cheshire GB SK11 9FT","c":"GB","o":"SKA Organisation"}]);
