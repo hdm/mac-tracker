a5548ea73ba57b71c5a8976ca223847611af533d
datagrinder <data@hdm.io>
2020-08-01 01:02:20 +0000

    Update 2020-08-01

---
+++ data/js/70/b3/d5/25/e0/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d525e000/36",[{"d":"2020-08-01","t":"add","s":"ieee-oui36.csv","a":"41-14, Burim-ro 170beon-gil Dongan-gu, Anyang-si Gyeonggi-do KR 14055","c":"KR","o":"RFHIC"}]);
--- data/js/70/b3/d5/b8/60/00.36.js
+++ data/js/70/b3/d5/b8/60/00.36.js
@@ -1,1 +1,1 @@
-deepmacDetailCallback("70b3d5b86000/36",[{"d":"2020-04-30","t":"add","s":"ieee-oui36.csv","a":"4-6 Kiprou str Tavros Athens GR 17778","c":"GR","o":"Cubitech"}]);
+deepmacDetailCallback("70b3d5b86000/36",[{"d":"2020-04-30","t":"add","s":"ieee-oui36.csv","a":"4-6 Kiprou str Tavros Athens GR 17778","c":"GR","o":"Cubitech"},{"d":"2020-08-01","t":"change","s":"ieee-oui36.csv","a":"4-6 Kiprou str Tavros Athens GR 17778","c":"GR","o":"Hilo"}]);
