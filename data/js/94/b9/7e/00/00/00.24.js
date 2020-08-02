d917413ff9536456ea420ca6813748d121a09a53
datagrinder <data@hdm.io>
2020-08-02 01:01:36 +0000

    Update 2020-08-02

---
+++ data/js/94/b9/7e/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("94b97e000000/24",[{"d":"2020-08-02","t":"add","s":"ieee-oui.csv","a":"Room 204, Building 2, 690 Bibo Rd, Pudong New Area Shanghai Shanghai CN 201203","c":"CN","o":"Espressif Inc."}]);
