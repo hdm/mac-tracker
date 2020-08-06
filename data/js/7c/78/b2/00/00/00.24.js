e516f68e7b28e42c5cd761df3b4c57722fc7d48a
datagrinder <data@hdm.io>
2020-08-06 01:02:07 +0000

    Update 2020-08-06

---
+++ data/js/7c/78/b2/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("7c78b2000000/24",[{"d":"2020-08-06","t":"add","s":"ieee-oui.csv","a":"4030 Lake Washington Boulevard NE Kirkland WA US 98033","c":"US","o":"Wyze Labs Inc"}]);
---
+++ data/js/cc/4f/5c/b0/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("cc4f5cb00000/28",[{"d":"2020-08-06","t":"add","s":"ieee-mam.csv","a":"Genthof 5 Buggenhout NA BE 9255","c":"BE","o":"Ontex BV"}]);
