0834e745b9f3d41dc6a253895ebfba4aa80bbc97
datagrinder <data@hdm.io>
2020-08-20 01:01:26 +0000

    Update 2020-08-20

---
+++ data/js/14/13/33/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("141333000000/24",[{"d":"2020-08-20","t":"add","s":"ieee-oui.csv","a":"8F., No. 94, Baozhong Rd. New Taipei City Taiwan TW 231","c":"TW","o":"AzureWave Technology Inc."}]);
---
+++ data/js/70/b3/d5/9a/60/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d59a6000/36",[{"d":"2020-08-20","t":"add","s":"ieee-oui36.csv","a":"Centenary Building, 2nd Floor,, East Wing, No. 28 M.G. Road BANGALORE KARNATAKA IN 560025","c":"IN","o":"QUNU LABS PRIVATE LIMITED"}]);
