5aafcba64647c5e928689c356e857850f8188931
datagrinder <data@hdm.io>
2020-08-27 01:01:25 +0000

    Update 2020-08-27

---
+++ data/js/7c/4e/09/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("7c4e09000000/24",[{"d":"2020-08-27","t":"add","s":"ieee-oui.csv","a":"Unit 3A01,Block A Skyworth Building,Gaoxin Ave.I.S.,Nanshan District Shenzhen Guangdong CN 518057","c":"CN","o":"Shenzhen Skyworth Wireless Technology Co.,Ltd"}]);
