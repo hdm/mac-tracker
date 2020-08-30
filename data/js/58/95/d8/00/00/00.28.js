cad8c45520186e56ae4693f097558ce5f716ab71
datagrinder <data@hdm.io>
2020-08-30 01:01:24 +0000

    Update 2020-08-30

---
+++ data/js/58/95/d8/00/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("5895d8000000/28",[{"d":"2020-08-30","t":"add","s":"ieee-mam.csv","a":"Shenzhen DOOGEE Hengtong Technology CO.,LTD Shenzhen Guangdong CN 518000","c":"CN","o":"Shenzhen DOOGEE Hengtong Technology CO.,LTD"}]);
---
+++ data/js/58/95/d8/20/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("5895d8200000/28",[{"d":"2020-08-30","t":"add","s":"ieee-mam.csv","a":"3F,No.81,Yu-Yih Rd.,Chu-Nan Chen Miao-Lih Hsuan  TW 115","c":"TW","o":"Sercomm Corporation."}]);
