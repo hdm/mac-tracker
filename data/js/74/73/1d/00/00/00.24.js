0d56cba778af23eb3002f7455ff09610739d37ba
datagrinder <data@hdm.io>
2020-09-01 01:01:24 +0000

    Update 2020-09-01

---
+++ data/js/74/73/1d/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("74731d000000/24",[{"d":"2020-09-01","t":"add","s":"ieee-oui.csv","a":"ifm-Straße 1 Tettnang BW DE 88069","c":"DE","o":"ifm electronic gmbh"}]);
---
+++ data/js/a8/b0/88/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("a8b088000000/24",[{"d":"2020-09-01","t":"add","s":"ieee-oui.csv","a":"660 3rd Street San Francisco CA US 94107","c":"US","o":"eero inc."}]);
