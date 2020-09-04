10862bfe0e23d41c8ae721ef426c165ede269b17
datagrinder <data@hdm.io>
2020-09-04 01:02:18 +0000

    Update 2020-09-04

---
+++ data/js/70/b3/d5/3d/10/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d53d1000/36",[{"d":"2020-09-04","t":"add","s":"ieee-oui36.csv","a":"Campus 1 Innovation Park, Balgownie Road Bridge of Don Aberdeen GB AB22 8GT","c":"GB","o":"Imenco Ltd"}]);
---
+++ data/js/e0/e1/a9/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("e0e1a9000000/24",[{"d":"2020-09-04","t":"add","s":"ieee-oui.csv","a":"Room 607-610, Block B, TAOJINDI Electronic Business Incubation Base Tenglong Road, Longhua District,  Shenzhen Guangdong CN 518000","c":"CN","o":"Shenzhen Four Seas Global Link Network Technology Co., Ltd."}]);
