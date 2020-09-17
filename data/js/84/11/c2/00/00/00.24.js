8575e5f15ae359d91d2371d3bd5a1226309a5d71
datagrinder <data@hdm.io>
2020-09-17 01:01:38 +0000

    Update 2020-09-17

---
+++ data/js/84/11/c2/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("8411c2000000/24",[{"d":"2020-09-17","t":"add","s":"ieee-oui.csv","a":"445 Hoes Lane Piscataway NJ US 08554","c":"US","o":"IEEE Registration Authority"}]);
---
+++ data/js/84/11/c2/30/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("8411c2300000/28",[{"d":"2020-09-17","t":"add","s":"ieee-mam.csv","a":"2-1,Shintoyofuta Kashiwa-shi Chiba JP 277-0804","c":"JP","o":"Hitachi,Ltd."}]);
