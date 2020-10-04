79a03f556a25f6ff0f890c40d27270e10c5b869f
datagrinder <data@hdm.io>
2020-10-04 01:01:33 +0000

    Update 2020-10-04

---
+++ data/js/f0/23/ae/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("f023ae000000/24",[{"d":"2020-10-04","t":"add","s":"ieee-oui.csv","a":"3F, No.15-1 Zhonghua Road, Hsinchu Industrail Park, Hukou, Hsinchu Hsinchu,Taiwan R.O.C. TW 30352","c":"TW","o":"AMPAK Technology,Inc."}]);
