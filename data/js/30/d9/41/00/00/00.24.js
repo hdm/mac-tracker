a1289cce57446a3443f12d7bd11e2b4aa829bd64
datagrinder <data@hdm.io>
2020-10-08 01:01:27 +0000

    Update 2020-10-08

---
+++ data/js/30/d9/41/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("30d941000000/24",[{"d":"2020-10-08","t":"add","s":"ieee-oui.csv","a":"2F, No. 23, LiHsin Rd., Hsinchu Science Park Hsinchu, Taiwan, R.O.C.  TW TW 300","c":"TW","o":"Raydium Semiconductor Corp."}]);
---
+++ data/js/8c/47/6e/90/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("8c476e900000/28",[{"d":"2020-10-08","t":"add","s":"ieee-mam.csv","a":"Horisontvagen 60 Stockholm Stockholm SE 12830","c":"SE","o":"Xertified AB"}]);
