4f001163e964c8909bf8711550f7065a062a688e
datagrinder <data@hdm.io>
2020-10-09 01:01:29 +0000

    Update 2020-10-09

--- data/js/00/50/c2/a2/a0/00.36.js
+++ data/js/00/50/c2/a2/a0/00.36.js
@@ -1,1 +1,1 @@
-deepmacDetailCallback("0050c2a2a000/36",[{"a":"6020 S 190th St Kent WA US 98032","o":"Custom Control Concepts","d":"2009-01-18","t":"add","s":"ieee","c":"US"}]);
+deepmacDetailCallback("0050c2a2a000/36",[{"a":"6020 S 190th St Kent WA US 98032","o":"Custom Control Concepts","d":"2009-01-18","t":"add","s":"ieee","c":"US"},{"d":"2020-10-09","t":"change","s":"ieee-iab.csv","a":"6020 S 190th St Kent WA US 98032","c":"US","o":"Astronics Custom Control Concepts"}]);
