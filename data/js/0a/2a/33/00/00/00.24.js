f33b95210185b51a61eae7987feafc5d118e2866
datagrinder <data@hdm.io>
2020-10-17 01:02:22 +0000

    Update 2020-10-17

---
+++ data/js/0a/2a/33/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("0a2a33000000/24",[{"d":"2020-10-17","t":"add","s":"ieee-cid.csv","a":"89 Dillon Ave Campbell CA US 95008","c":"US","o":"DIGISTOR"}]);
---
+++ data/js/70/b3/d5/c2/80/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d5c28000/36",[{"d":"2020-10-17","t":"add","s":"ieee-oui36.csv","a":"219 Robert Hicks Dr Toronto ON CA M2R3R3","c":"CA","o":"Mitech Integrated Systems Inc."}]);
