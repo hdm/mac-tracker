5ef3846d3aefe051be8b987dc303d5375131d9f8
datagrinder <data@hdm.io>
2020-10-23 01:01:29 +0000

    Update 2020-10-23

---
+++ data/js/58/b0/fe/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("58b0fe000000/24",[{"d":"2020-10-23","t":"add","s":"ieee-oui.csv","a":"Am Herdicksbach 2 - Halle 9 Waltrop  DE 45731","c":"DE","o":"Team EPS GmbH"}]);
---
+++ data/js/a4/53/ee/00/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("a453ee000000/28",[{"d":"2020-10-23","t":"add","s":"ieee-mam.csv","a":"Ctra. Madrid-Valencia Km. 196 Motilla del Palancar Cuenca ES 16200","c":"ES","o":"MASBER ELECTRIC SL"}]);
