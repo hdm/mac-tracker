6e495a75afafdbe8ca86c05f591f033412277627
datagrinder <data@hdm.io>
2020-11-06 01:01:24 +0000

    Update 2020-11-06

---
+++ data/js/48/51/c5/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("4851c5000000/24",[{"d":"2020-11-06","t":"add","s":"ieee-oui.csv","a":"Lot 8, Jalan Hi-Tech 2/3   Kulim Kedah MY 09000","c":"MY","o":"Intel Corporate"}]);
---
+++ data/js/70/b3/d5/88/70/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d5887000/36",[{"d":"2020-11-06","t":"add","s":"ieee-oui36.csv","a":"C/ Bambú 4 Madrid Madrid ES 28036","c":"ES","o":"Entec Solar S.L."}]);
