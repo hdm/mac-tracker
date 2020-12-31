6b3202dfabcc12bfd8be97b6138ae515161ba6f1
datagrinder <data@hdm.io>
2020-12-31 01:01:31 +0000

    Update 2020-12-31

---
+++ data/js/70/b3/d5/21/20/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d5212000/36",[{"d":"2020-12-31","t":"add","s":"ieee-oui36.csv","a":"83 PINE HILL RD SOUTHBOROUGH MA US 01772","c":"US","o":"Semiconsoft, inc"}]);
---
+++ data/js/70/b3/d5/91/50/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d5915000/36",[{"d":"2020-12-31","t":"add","s":"ieee-oui36.csv","a":"13873 Park Center Rd, Ste 510N Herndon VA US 20171","c":"US","o":"DHK Storage, LLC"}]);
