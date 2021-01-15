934ec1a39712522ef003f8f69f043f40bb97e25a
datagrinder <data@hdm.io>
2021-01-15 01:01:33 +0000

    Update 2021-01-15

---
+++ data/js/60/15/92/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("601592000000/24",[{"d":"2021-01-15","t":"add","s":"ieee-oui.csv","a":"445 Hoes Lane Piscataway NJ US 08554","c":"US","o":"IEEE Registration Authority"}]);
---
+++ data/js/84/23/88/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("842388000000/24",[{"d":"2021-01-15","t":"add","s":"ieee-oui.csv","a":"350 West Java Drive Sunnyvale CA US 94089","c":"US","o":"Ruckus Wireless"}]);
