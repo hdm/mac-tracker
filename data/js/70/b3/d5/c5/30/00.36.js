b059496dc237be968484eec1978c6bace54599eb
datagrinder <data@hdm.io>
2021-01-17 01:01:34 +0000

    Update 2021-01-17

--- data/js/70/b3/d5/c5/30/00.36.js
+++ data/js/70/b3/d5/c5/30/00.36.js
@@ -1,1 +1,1 @@
-deepmacDetailCallback("70b3d5c53000/36",[{"a":"Jasnogórska, 44 Kraków Lesser Poland PL 31-358","o":"S Labs sp. z o.o.","d":"2017-05-21","t":"add","s":"ieee","c":"PL"}]);
+deepmacDetailCallback("70b3d5c53000/36",[{"a":"Jasnogórska, 44 Kraków Lesser Poland PL 31-358","o":"S Labs sp. z o.o.","d":"2017-05-21","t":"add","s":"ieee","c":"PL"},{"d":"2021-01-17","t":"change","s":"ieee-oui36.csv","a":"Dworska 1a/1u Kraków Lesser Poland PL 30-314","c":"PL","o":"S Labs sp. z o.o."}]);
