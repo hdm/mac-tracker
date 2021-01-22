81d68ac15ec2c9d906f335b4b029176dff7e9290
datagrinder <data@hdm.io>
2021-01-22 01:01:30 +0000

    Update 2021-01-22

---
+++ data/js/2c/be/eb/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("2cbeeb000000/24",[{"d":"2021-01-22","t":"add","s":"ieee-oui.csv","a":"11 Staple Inn London London GB WC1V 7QH","c":"GB","o":"Nothing Technology Limited"}]);
---
+++ data/js/74/09/ac/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("7409ac000000/24",[{"d":"2021-01-22","t":"add","s":"ieee-oui.csv","a":"5214 68th St., Suite 201 Lubbock TX US 79424","c":"US","o":"Quext, LLC"}]);
