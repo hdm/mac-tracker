018c2b10008ec7d9da92373b8c1e0541c4cbdd9d
datagrinder <data@hdm.io>
2021-01-29 01:02:32 +0000

    Update 2021-01-29

---
+++ data/js/0c/5c/b5/20/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("0c5cb5200000/28",[{"d":"2021-01-29","t":"add","s":"ieee-mam.csv","a":"B1715,Jiansheng Building,No. 1 Pingji Road, NanWan Street Longgang Shenzhen  CN 518112","c":"CN","o":"HongKong Blossom Limited"}]);
---
+++ data/js/4c/77/66/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("4c7766000000/24",[{"d":"2021-01-29","t":"add","s":"ieee-oui.csv","a":"3/F, Building R1-B, High-Tech Industrial Park, Nanshan District Shenzhen Guangdong CN 518057","c":"CN","o":"SHENZHEN MERCURY COMMUNICATION TECHNOLOGIES CO.,LTD."}]);
