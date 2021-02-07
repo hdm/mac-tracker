4b1836a0836d10a612e95bd80adf53fd3f027ffb
datagrinder <data@hdm.io>
2021-02-07 01:01:39 +0000

    Update 2021-02-07

---
+++ data/js/70/b3/d5/03/60/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d5036000/36",[{"d":"2021-02-07","t":"add","s":"ieee-oui36.csv","a":"Les Aubépins ARGENT SUR SAULDRE CHER FR 18410","c":"FR","o":"PARAGON ID"}]);
---
+++ data/js/70/b3/d5/e1/90/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d5e19000/36",[{"d":"2021-02-07","t":"add","s":"ieee-oui36.csv","a":"Hoerder Burgstr.18 Dortmund NRW DE 44263","c":"DE","o":"BAB TECHNOLOGIE GmbH"}]);
