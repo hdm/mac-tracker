06d1ecceaeffb153836701ef6b5589d60a370f59
datagrinder <data@hdm.io>
2021-02-09 01:02:14 +0000

    Update 2021-02-09

---
+++ data/js/78/f2/35/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("78f235000000/24",[{"d":"2021-02-09","t":"add","s":"ieee-oui.csv","a":"Anzhou, Industrial Park Mianyang Sichuan CN 622650","c":"CN","o":"Sichuan AI-Link Technology Co., Ltd."}]);
