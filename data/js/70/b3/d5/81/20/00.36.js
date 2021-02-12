7da6c8ababb5e2b940828401ba97f6534cc981ea
datagrinder <data@hdm.io>
2021-02-12 01:01:30 +0000

    Update 2021-02-12

---
+++ data/js/70/b3/d5/81/20/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d5812000/36",[{"d":"2021-02-12","t":"add","s":"ieee-oui36.csv","a":"Libusina tr.1 Brno  CZ 62300","c":"CZ","o":"TESCAN Brno, s.r.o."}]);
--- data/js/70/b3/d5/d4/50/00.36.js
+++ data/js/70/b3/d5/d4/50/00.36.js
@@ -1,1 +1,1 @@
-deepmacDetailCallback("70b3d5d45000/36",[{"d":"2021-02-04","t":"add","s":"ieee-oui36.csv","a":"Biala 1 Gdansk pomorskie PL 80-435","c":"PL","o":"Vemco Sp. z o. o."}]);
+deepmacDetailCallback("70b3d5d45000/36",[{"d":"2021-02-04","t":"add","s":"ieee-oui36.csv","a":"Biala 1 Gdansk pomorskie PL 80-435","c":"PL","o":"Vemco Sp. z o. o."},{"d":"2021-02-12","t":"change","s":"ieee-oui36.csv","a":"ul. Biala 1 Gdansk  PL 80-435","c":"PL","o":"Vemco Sp. z o. o."}]);
