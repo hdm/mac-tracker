075da6353e3ad8da3961b357d909b85488c827b3
datagrinder <data@hdm.io>
2021-02-13 01:01:52 +0000

    Update 2021-02-13

--- data/js/00/50/c2/f3/c0/00.36.js
+++ data/js/00/50/c2/f3/c0/00.36.js
@@ -1,1 +1,1 @@
-deepmacDetailCallback("0050c2f3c000/36",[{"a":"ul. Broniewskiego 3 Sopot pomorskie US 81-837","o":"Vemco Sp. z o. o.","d":"2012-02-19","t":"add","s":"ieee","c":"US"}]);
+deepmacDetailCallback("0050c2f3c000/36",[{"a":"ul. Broniewskiego 3 Sopot pomorskie US 81-837","o":"Vemco Sp. z o. o.","d":"2012-02-19","t":"add","s":"ieee","c":"US"},{"d":"2021-02-13","t":"change","s":"ieee-iab.csv","a":"ul. Biala 1 Gdansk  PL 80-435","c":"PL","o":"Vemco Sp. z o. o."}]);
