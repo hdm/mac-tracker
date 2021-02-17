9f6e881f7cb2159329ec2ffe0519b055def4800e
datagrinder <data@hdm.io>
2021-02-17 01:02:09 +0000

    Update 2021-02-17

---
+++ data/js/3c/37/12/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("3c3712000000/24",[{"d":"2021-02-17","t":"add","s":"ieee-oui.csv","a":"Alt-Moabit 95 Berlin Berlin DE 10559","c":"DE","o":"AVM Audiovisuelles Marketing und Computersysteme GmbH"}]);
