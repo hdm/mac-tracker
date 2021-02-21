2f694a1d2ee0aecaed10436c4c94459f200ad743
datagrinder <data@hdm.io>
2021-02-21 01:01:49 +0000

    Update 2021-02-21

---
+++ data/js/20/ce/2a/60/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("20ce2a600000/28",[{"d":"2021-02-21","t":"add","s":"ieee-mam.csv","a":"Kwekerijweg, 2A Zeist Nederland NL 3709 JA","c":"JA","o":"Radarxense BV"}]);
---
+++ data/js/70/b3/d5/1f/b0/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d51fb000/36",[{"d":"2021-02-21","t":"add","s":"ieee-oui36.csv","a":"Tamakushicho-higashi 3-3-97 Higashi osaka OSAKA JP 5780932","c":"JP","o":"Crane-elec. Co., LTD."}]);
