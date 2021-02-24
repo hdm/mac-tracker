0f6cfdffff4fe6dcffe78efce546902b3c3372ee
datagrinder <data@hdm.io>
2021-02-24 01:01:39 +0000

    Update 2021-02-24

---
+++ data/js/10/1e/da/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("101eda000000/24",[{"d":"2021-02-24","t":"add","s":"ieee-oui.csv","a":"28-32 Boulevard de Grenelle PARIS  FR 75015","c":"FR","o":"INGENICO TERMINALS SAS"}]);
---
+++ data/js/54/ae/d2/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("54aed2000000/24",[{"d":"2021-02-24","t":"add","s":"ieee-oui.csv","a":"Salamander Quay West, Park Lane Harefield Middlesex GB UB9 6NZ","c":"GB","o":"CSL Dualcom Ltd"}]);
