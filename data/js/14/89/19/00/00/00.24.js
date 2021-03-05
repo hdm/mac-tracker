0ce3f7896b4b7be80a2aa26f860b2d4689e1a1a5
datagrinder <data@hdm.io>
2021-03-05 01:01:38 +0000

    Update 2021-03-05

---
+++ data/js/14/89/19/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("148919000000/24",[{"d":"2021-03-05","t":"add","s":"ieee-oui.csv","a":"#1502 , T-dong, Pungrim I-want, 170, Seohyeon-ro Seongnam-si Gyeonggi-do KR 13590","c":"KR","o":"2bps"}]);
