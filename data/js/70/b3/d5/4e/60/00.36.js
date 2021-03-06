827ffd11ac08443eb9e43a251aa6711c279342dd
datagrinder <data@hdm.io>
2021-03-06 01:01:52 +0000

    Update 2021-03-06

---
+++ data/js/70/b3/d5/4e/60/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d54e6000/36",[{"d":"2021-03-06","t":"add","s":"ieee-oui36.csv","a":"340 Storke Road, Suite 101 Goleta CA US 93117","c":"US","o":"Santa Barbara Imaging Systems"}]);
---
+++ data/js/dc/03/98/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("dc0398000000/24",[{"d":"2021-03-06","t":"add","s":"ieee-oui.csv","a":"26, Hanamsandan 5beon-ro Gwangju Gwangsan-gu KR 506-731","c":"KR","o":"LG Innotek"}]);
