b447a240c156a0d5adfe419f6582791aad0a4569
datagrinder <data@hdm.io>
2021-03-10 01:01:42 +0000

    Update 2021-03-10

---
+++ data/js/40/c4/8c/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("40c48c000000/24",[{"d":"2021-03-10","t":"add","s":"ieee-oui.csv","a":"NiTUS 85, Deokcheon-ro Anyang-si Gyeonggi-do, Korea KR 14086","c":"KR","o":"N-iTUS CO.,LTD."}]);
---
+++ data/js/70/b3/d5/a6/30/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("70b3d5a63000/36",[{"d":"2021-03-10","t":"add","s":"ieee-oui36.csv","a":"Unit 6 Christchurch New Zealand NZ 8011","c":"NZ","o":"DesignA Electronics Limited"}]);
