40dc226857a6a15a8f0e3e33dd248442d0e841d6
datagrinder <data@hdm.io>
2021-03-11 01:01:35 +0000

    Update 2021-03-11

---
+++ data/js/98/49/9f/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("98499f000000/24",[{"d":"2021-03-11","t":"add","s":"ieee-oui.csv","a":"DTC Fusion 2, 1100 Parkway Whiteley Hampshire GB PO15 7AB","c":"GB","o":"Domo Tactical Communications"}]);
