8c1d45f7ec947746bfcfe4ed83a530ec00dd1be7
datagrinder <data@hdm.io>
2021-03-27 01:01:53 +0000

    Update 2021-03-27

---
+++ data/js/d0/9f/d9/30/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("d09fd9300000/28",[{"d":"2021-03-27","t":"add","s":"ieee-mam.csv","a":"677 Shimoakasaka-Ohnohara Kawagoe-Shi Saitama JP 350-1155","c":"JP","o":"Sanken-Densetsu Co.,LTD."}]);
