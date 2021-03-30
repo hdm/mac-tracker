7bfefd6377bb8c61afa1f9eae4142d34ec658020
datagrinder <data@hdm.io>
2021-03-30 01:01:42 +0000

    Update 2021-03-30

---
+++ data/js/88/ae/dd/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("88aedd000000/24",[{"d":"2021-03-30","t":"add","s":"ieee-oui.csv","a":"No. 239, Sec. 2, Ti ding Blvd. Taipei City  TW 11493","c":"TW","o":"EliteGroup Computer Systems Co., LTD"}]);
