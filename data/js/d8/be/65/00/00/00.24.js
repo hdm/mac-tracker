9f7365a0c5c49ef338b23796d9a7e7b021028c66
datagrinder <data@hdm.io>
2021-04-07 01:01:56 +0000

    Update 2021-04-07

---
+++ data/js/d8/be/65/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("d8be65000000/24",[{"d":"2021-04-07","t":"add","s":"ieee-oui.csv","a":"P.O Box 8102  Reno NV US 89507","c":"US","o":"Amazon Technologies Inc."}]);
---
+++ data/js/e8/78/29/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("e87829000000/24",[{"d":"2021-04-07","t":"add","s":"ieee-oui.csv","a":"445 Hoes Lane Piscataway NJ US 08554","c":"US","o":"IEEE Registration Authority"}]);
