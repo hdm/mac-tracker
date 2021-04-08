5bb8026937f91625ecb6fa495096173501b0efb5
datagrinder <data@hdm.io>
2021-04-08 01:02:02 +0000

    Update 2021-04-08

---
+++ data/js/7c/10/c9/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("7c10c9000000/24",[{"d":"2021-04-08","t":"add","s":"ieee-oui.csv","a":"15,Li-Te Rd., Peitou, Taipei 112, Taiwan Taipei Taiwan TW 112","c":"TW","o":"ASUSTek COMPUTER INC."}]);
