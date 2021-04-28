21004bb62b4e19b2ac791fd4db512a088794bfc9
datagrinder <data@hdm.io>
2021-04-28 01:01:55 +0000

    Update 2021-04-28

---
+++ data/js/70/1a/d5/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("701ad5000000/24",[{"d":"2021-04-28","t":"add","s":"ieee-oui.csv","a":"13428 Maxella Ave, #866 Marina Del Rey CA US 90292","c":"US","o":"Openpath Security, Inc."}]);
---
+++ data/js/8c/1f/64/94/70/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("8c1f64947000/36",[{"d":"2021-04-28","t":"add","s":"ieee-oui36.csv","a":"Bolshaya Serpukhovskaya street 25, bld. 2, 1 st floor, room III Moscow  RU 115093","c":"RU","o":"LLC \"TC \"Vympel\""}]);
