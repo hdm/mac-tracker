04c57d50124228119d6a55b23534c00f91794fe7
datagrinder <data@hdm.io>
2021-05-12 01:02:05 +0000

    Update 2021-05-12

---
+++ data/js/f0/2a/2b/b0/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("f02a2bb00000/28",[{"d":"2021-05-12","t":"add","s":"ieee-mam.csv","a":"Via Pontarola, 70 Campodarsego Padua IT I35011","c":"IT","o":"EL.MO. spa"}]);
---
+++ data/js/f0/2a/2b/c0/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("f02a2bc00000/28",[{"d":"2021-05-12","t":"add","s":"ieee-mam.csv","a":"Eisenberger Straße 56a Kerzenheim  DE 67304","c":"DE","o":"Comexio GmbH"}]);
