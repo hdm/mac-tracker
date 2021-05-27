38011c46a64be0b4926f0c248b5a8ea3221b09b3
datagrinder <data@hdm.io>
2021-05-27 01:01:58 +0000

    Update 2021-05-27

---
+++ data/js/58/48/49/d0/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("584849d00000/28",[{"d":"2021-05-27","t":"add","s":"ieee-mam.csv","a":"Hofaeckerstr. 18 Crailsheim Baden-Wuerttemberg DE 74564","c":"DE","o":"Telegaertner Elektronik GmbH"}]);
---
+++ data/js/8c/1f/64/41/40/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("8c1f64414000/36",[{"d":"2021-05-27","t":"add","s":"ieee-oui36.csv","a":"Am Weichselgarten 7 Erlangen  DE D-91058","c":"DE","o":"INSEVIS GmbH"}]);
