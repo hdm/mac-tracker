8848c31431eac3ca7cb18bb5f1f99668e4c9cc3d
datagrinder <data@hdm.io>
2021-06-02 01:02:29 +0000

    Update 2021-06-02

---
+++ data/js/58/48/49/10/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("584849100000/28",[{"d":"2021-06-02","t":"add","s":"ieee-mam.csv","a":"Rosenkaeret 11C Soeborg  DK 2860","c":"DK","o":"SKAARHOJ ApS"}]);
---
+++ data/js/8c/f6/81/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("8cf681000000/24",[{"d":"2021-06-02","t":"add","s":"ieee-oui.csv","a":"400 West Cesar Chavez Street Austin TX US 78701","c":"US","o":"Silicon Laboratories"}]);
