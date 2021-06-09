3d26728fffa8a56547e98083b71e0c100c22ef59
datagrinder <data@hdm.io>
2021-06-09 01:01:30 +0000

    Update 2021-06-09

---
+++ data/js/5c/75/c6/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("5c75c6000000/24",[{"d":"2021-06-09","t":"add","s":"ieee-oui.csv","a":"32 Xuanwumen West Street,Xicheng District Beijing  CN 100053","c":"CN","o":"China Mobile Group Device Co.,Ltd."}]);
