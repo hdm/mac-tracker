4de3cf218c0ba6c5b0b2f6fafb8af43b4d695e15
datagrinder <data@hdm.io>
2021-06-13 01:02:16 +0000

    Update 2021-06-13

---
+++ data/js/54/6f/71/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("546f71000000/24",[{"d":"2021-06-13","t":"add","s":"ieee-oui.csv","a":"300 Pine Needle Lane Bigfork MT US 59911","c":"US","o":"uAvionix Corporation"}]);
---
+++ data/js/64/33/b5/40/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("6433b5400000/28",[{"d":"2021-06-13","t":"add","s":"ieee-mam.csv","a":"4611 Bee Cave Rd Suite 200 austin TX US 78746","c":"US","o":"Eagle Eye Networks, Inc"}]);
