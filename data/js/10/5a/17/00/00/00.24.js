0fbbed455d3cc47681f5aa6bef14871d50670386
datagrinder <data@hdm.io>
2021-06-25 01:02:02 +0000

    Update 2021-06-25

---
+++ data/js/10/5a/17/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("105a17000000/24",[{"d":"2021-06-25","t":"add","s":"ieee-oui.csv","a":"160 Greentree Drive, Suite 101 Dover DE US 19904","c":"US","o":"Tuya Smart Inc."}]);
---
+++ data/js/94/64/24/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("946424000000/24",[{"d":"2021-06-25","t":"add","s":"ieee-oui.csv","a":"3333 Scott Blvd Santa Clara CA US 95054","c":"US","o":"Aruba, a Hewlett Packard Enterprise Company"}]);
