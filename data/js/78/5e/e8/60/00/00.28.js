d18b5314806ddb59ad16f1c4ff84ae12d87c0dd4
datagrinder <data@hdm.io>
2021-06-29 01:01:30 +0000

    Update 2021-06-29

---
+++ data/js/78/5e/e8/60/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("785ee8600000/28",[{"d":"2021-06-29","t":"add","s":"ieee-mam.csv","a":"ROOM 601 ROOM 701,BLD.2,NO.2,SCIENCE AND TECHNOLOGY 9 RD,SONGSHAN LAKE HI-TECH ZONE Dongguan Guangdong CN 523808","c":"CN","o":"Guangdong COROS Sports Technology Co., Ltd"}]);
