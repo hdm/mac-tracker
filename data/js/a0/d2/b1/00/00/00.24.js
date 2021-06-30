3c1bf626f81a6cc36f46f5c068829e6c4721b669
datagrinder <data@hdm.io>
2021-06-30 01:01:47 +0000

    Update 2021-06-30

---
+++ data/js/a0/d2/b1/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("a0d2b1000000/24",[{"d":"2021-06-30","t":"add","s":"ieee-oui.csv","a":"P.O Box 8102  Reno NV US 89507","c":"US","o":"Amazon Technologies Inc."}]);
---
+++ data/js/ec/1c/5d/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("ec1c5d000000/24",[{"d":"2021-06-30","t":"add","s":"ieee-oui.csv","a":"Werner-von-Siemens-Str. 50 Amberg  DE 92224","c":"DE","o":"Siemens AG"}]);
