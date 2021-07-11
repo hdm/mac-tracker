32128fe02962fa9b53b981046fbaaf0ed3bb808e
datagrinder <data@hdm.io>
2021-07-11 01:01:56 +0000

    Update 2021-07-11

--- data/js/8c/1f/64/c7/c0/00.36.js
+++ data/js/8c/1f/64/c7/c0/00.36.js
@@ -1,1 +1,1 @@
-deepmacDetailCallback("8c1f64c7c000/36",[{"d":"2021-07-09","t":"add","s":"ieee-oui36.csv","a":"Industriestraße 3 Kötz  DE 89359","c":"DE","o":"Merkle Schweißanlagentechnik GmbH"}]);
+deepmacDetailCallback("8c1f64c7c000/36",[{"d":"2021-07-09","t":"add","s":"ieee-oui36.csv","a":"Industriestraße 3 Kötz  DE 89359","c":"DE","o":"Merkle Schweißanlagentechnik GmbH"},{"d":"2021-07-11","t":"change","s":"ieee-oui36.csv","a":"Industriestraße 3 Kötz   DE 89359","c":"DE","o":"MERKLE Schweissanlagen-Technik GmbH"}]);
---
+++ data/js/c4/fc/ef/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("c4fcef000000/24",[{"d":"2021-07-11","t":"add","s":"ieee-oui.csv","a":"2100 Geng Rd #103 Palo Alto  US 94303","c":"US","o":"SambaNova Systems, Inc."}]);
