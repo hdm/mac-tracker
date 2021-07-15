ab3721176720ae86072be9a0884c1241983780c4
datagrinder <data@hdm.io>
2021-07-15 01:01:28 +0000

    Update 2021-07-15

---
+++ data/js/2c/53/d7/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("2c53d7000000/24",[{"d":"2021-07-15","t":"add","s":"ieee-oui.csv","a":"Laubisruetistrasse 28 Staefa  CH 8712","c":"CH","o":"Sonova AG"}]);
---
+++ data/js/38/a8/cd/a0/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("38a8cda00000/28",[{"d":"2021-07-15","t":"add","s":"ieee-mam.csv","a":"2/5 Selezneva Street Krasnodar Krasnodarsky kray RU 350059","c":"RU","o":"NIC Technologii"}]);
