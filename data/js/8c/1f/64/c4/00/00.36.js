0a767e2a3d84d4ad97f8f59b6c26c50392fbfbd9
datagrinder <data@hdm.io>
2021-08-03 01:01:31 +0000

    Update 2021-08-03

---
+++ data/js/8c/1f/64/c4/00/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("8c1f64c40000/36",[{"d":"2021-08-03","t":"add","s":"ieee-oui36.csv","a":"Leipziger Str. 43b Bennwitz Deutschland DE 04828","c":"DE","o":"Sciospec Scientific Instruments GmbH"}]);
