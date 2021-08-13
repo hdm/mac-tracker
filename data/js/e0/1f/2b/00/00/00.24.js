d6d424df63d2f4980c20c7be98f3d0b33fc51fd0
datagrinder <data@hdm.io>
2021-08-13 01:01:45 +0000

    Update 2021-08-13

---
+++ data/js/e0/1f/2b/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("e01f2b000000/24",[{"d":"2021-08-13","t":"add","s":"ieee-oui.csv","a":"Werinherstrasse 91 München Bavaria DE D-81541","c":"DE","o":"Nokia Solutions and Networks GmbH & Co. KG"}]);
