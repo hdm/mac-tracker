9c7a9b13cf2670a4a05738950def1967a7d6ac94
datagrinder <data@hdm.io>
2021-08-19 01:02:36 +0000

    Update 2021-08-19

---
+++ data/js/34/98/b5/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("3498b5000000/24",[{"d":"2021-08-19","t":"add","s":"ieee-oui.csv","a":"350 East Plumeria Drive San Jose CA US 95134","c":"US","o":"NETGEAR"}]);
---
+++ data/js/98/80/bb/50/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("9880bb500000/28",[{"d":"2021-08-19","t":"add","s":"ieee-mam.csv","a":"Transportstraat 1 Tessenderlo Select State BE 3980","c":"BE","o":"Melexis Technologies NV"}]);
