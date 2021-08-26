c0f572cf8a09213e1521df93250317acc95521fb
datagrinder <data@hdm.io>
2021-08-26 01:02:05 +0000

    Update 2021-08-26

---
+++ data/js/8c/1f/64/9f/00/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("8c1f649f0000/36",[{"d":"2021-08-26","t":"add","s":"ieee-oui36.csv","a":"350 2nd St, STE 6 Los Altos CA US 94022","c":"US","o":"ePlant, Inc."}]);
---
+++ data/js/a0/29/19/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("a02919000000/24",[{"d":"2021-08-26","t":"add","s":"ieee-oui.csv","a":"One Dell Way Round Rock TX US 78682","c":"US","o":"Dell Inc."}]);
