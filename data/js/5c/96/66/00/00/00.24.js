2a4b96a5157a771e24cabeeff99cd409a0876d2b
datagrinder <data@hdm.io>
2021-08-29 01:01:33 +0000

    Update 2021-08-29

---
+++ data/js/5c/96/66/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("5c9666000000/24",[{"d":"2021-08-29","t":"add","s":"ieee-oui.csv","a":"1-7-1 Konan Minato-ku Tokyo JP 108-0075","c":"JP","o":"Sony Interactive Entertainment Inc."}]);
