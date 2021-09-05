583a487169b744e59cb88001ec1d7f943a5d1f4e
datagrinder <data@hdm.io>
2021-09-05 01:01:46 +0000

    Update 2021-09-05

---
+++ data/js/6c/c4/9f/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("6cc49f000000/24",[{"d":"2021-09-05","t":"add","s":"ieee-oui.csv","a":"3333 Scott Blvd Santa Clara CA US 95054","c":"US","o":"Aruba, a Hewlett Packard Enterprise Company"}]);
