0a82188fbb23a7e7823ca7f9cfd9520d017ac91f
datagrinder <data@hdm.io>
2021-09-12 01:01:48 +0000

    Update 2021-09-12

---
+++ data/js/08/6f/48/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("086f48000000/24",[{"d":"2021-09-12","t":"add","s":"ieee-oui.csv","a":"Room 504A,Block B,Digital Building,Gargen City,No.1079,Nanhai Road,Nanshan District,Shenzhen. Shenzhen  CN 518067","c":"CN","o":"Shenzhen iComm Semiconductor CO.,LTD"}]);
---
+++ data/js/38/4b/24/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("384b24000000/24",[{"d":"2021-09-12","t":"add","s":"ieee-oui.csv","a":"Oestliche Rheinbrückenstraße 50 Karlsruhe Baden-Württemberg DE 76181","c":"DE","o":"SIEMENS AG"}]);
