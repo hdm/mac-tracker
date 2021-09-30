5329f7144bdc7c921cc58120aed631449a3ea570
datagrinder <data@hdm.io>
2021-09-30 01:02:21 +0000

    Update 2021-09-30

---
+++ data/js/48/02/86/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("480286000000/24",[{"d":"2021-09-30","t":"add","s":"ieee-oui.csv","a":"No.178 Yulong Avenue, Yufengshan, Yubei District, Chongqing. Chongqing  China CN 401120","c":"CN","o":"Realme Chongqing Mobile Telecommunications Corp.,Ltd."}]);
---
+++ data/js/8c/1f/64/ce/e0/00.36.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("8c1f64cee000/36",[{"d":"2021-09-30","t":"add","s":"ieee-oui36.csv","a":"Rua Soldado  Manuel Pinheiro Magalhãe, 68s Adaúfe Braga PT 4710-167","c":"PT","o":"DISPLAX S.A."}]);
