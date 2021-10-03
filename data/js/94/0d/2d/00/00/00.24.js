247bbab357e782b3429f043e3503e6884c921851
datagrinder <data@hdm.io>
2021-10-03 01:01:47 +0000

    Update 2021-10-03

---
+++ data/js/94/0d/2d/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("940d2d000000/24",[{"d":"2021-10-03","t":"add","s":"ieee-oui.csv","a":"201 E. Sandpointe Ave Santa Ana CA US 92707","c":"US","o":"Universal Electronics, Inc."}]);
