64d98180fd5f34ba7eb897363f58db7f38a4b9bf
datagrinder <data@hdm.io>
2021-10-15 01:01:46 +0000

    Update 2021-10-15

---
+++ data/js/08/f8/0d/90/00/00.28.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("08f80d900000/28",[{"d":"2021-10-15","t":"add","s":"ieee-mam.csv","a":"6F, No.95, Xinhu 1st Rd., Neihu Dist. Taipei  TW 114","c":"TW","o":"Benelink Technology Inc."}]);
---
+++ data/js/60/56/99/00/00/00.24.js
@@ -0,0 +1,1 @@
+deepmacDetailCallback("605699000000/24",[{"d":"2021-10-15","t":"add","s":"ieee-oui.csv","a":"","c":"","o":"Private"}]);
