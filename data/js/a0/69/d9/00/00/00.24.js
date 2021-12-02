0e746d9186f6c58fc4581d8d07dfa5aa2bd4a225
datagrinder <data@hdm.io>
2021-12-02 01:02:50 +0000

    Update 2021-12-02

---
+++ data/js/a0/69/d9/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("a069d9000000/24",[{"d":"2021-12-02","t":"add","s":"ieee-oui.csv","a":"466 Changhe Road, Binjiang District Hangzhou Zhejiang CN 310052","c":"CN","o":"New H3C Technologies Co., Ltd"}]);
