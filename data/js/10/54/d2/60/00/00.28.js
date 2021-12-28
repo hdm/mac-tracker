644a61587ace9f9cde4775f145f9f7b95dacfe82
datagrinder <data@hdm.io>
2021-12-28 01:01:45 +0000

    Update 2021-12-28

---
+++ data/js/10/54/d2/60/00/00.28.js
@@ -0,0 +1,1 @@
+macDetailCallback("1054d2600000/28",[{"d":"2021-12-28","t":"add","s":"ieee-mam.csv","a":"#B2, Zhongbaotong Creative Park Changfa West Road No.34 Bantian Shenzhen  CN 518029","c":"CN","o":"Lanao Communication Technology Limited"}]);
---
+++ data/js/8c/1f/64/9d/40/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f649d4000/36",[{"d":"2021-12-28","t":"add","s":"ieee-oui36.csv","a":"5007 Highland Ct #WPL-IEEE Austin TX US 78731","c":"US","o":"Wolfspyre Labs"}]);
