43d1348e052fc8e7e226563de5e3e2b0a2960cb1
datagrinder <data@hdm.io>
2022-01-05 01:02:11 +0000

    Update 2022-01-05

---
+++ data/js/30/bb/7d/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("30bb7d000000/24",[{"d":"2022-01-05","t":"add","s":"ieee-oui.csv","a":"18C02, 18C03, 18C04 ,18C05,TAIRAN BUILDING, Shenzhen Guangdong CN 518000","c":"CN","o":"OnePlus Technology (Shenzhen) Co., Ltd"}]);
---
+++ data/js/8c/1f/64/42/90/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f64429000/36",[{"d":"2022-01-05","t":"add","s":"ieee-oui36.csv","a":"P. O.  Box 6863 Rodeløkka Oslo Oslo NO 0504","c":"NO","o":"Abbott Diagnostics Technologies AS"}]);
