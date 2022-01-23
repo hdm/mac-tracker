d42d1ed25605cf7d88ae323b483d228ac7fc1c63
datagrinder <data@hdm.io>
2022-01-23 01:02:29 +0000

    Update 2022-01-23

---
+++ data/js/8c/1f/64/8d/90/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f648d9000/36",[{"d":"2022-01-23","t":"add","s":"ieee-oui36.csv","a":"Via Armenia, 16 San Vito al Tagliamento (PN)  IT 33078","c":"IT","o":"Pietro Fiorentini Spa"}]);
---
+++ data/js/8c/1f/64/eb/50/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f64eb5000/36",[{"d":"2022-01-23","t":"add","s":"ieee-oui36.csv","a":"38-23 higashi maeda Nishin City Aichi JP 470-0124","c":"JP","o":"Meiryo Denshi Corp."}]);
