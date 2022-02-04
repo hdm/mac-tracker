2f5e55c5a0799a3c08219d92b1cf230b0f08a66f
datagrinder <data@hdm.io>
2022-02-04 01:02:35 +0000

    Update 2022-02-04

---
+++ data/js/8c/1f/64/e6/40/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f64e64000/36",[{"d":"2022-02-04","t":"add","s":"ieee-oui36.csv","a":"Ka-211, Whangmool ro 190, DongDaemoon Gu  Seoul  KR 02622","c":"KR","o":"Indefac company"}]);
---
+++ data/js/8c/1f/64/f2/70/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f64f27000/36",[{"d":"2022-02-04","t":"add","s":"ieee-oui36.csv","a":"Gerberstrasse 49 Backnang  DE 71522","c":"DE","o":"Tesat-Spacecom GmbH & Co. KG"}]);
