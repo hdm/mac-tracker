c777c471aac51d63a66d093eb496b34b56cc571c
datagrinder <data@hdm.io>
2022-02-06 01:02:48 +0000

    Update 2022-02-06

---
+++ data/js/8c/1f/64/4e/50/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f644e5000/36",[{"d":"2022-02-06","t":"add","s":"ieee-oui36.csv","a":"Renukas Castle, 35th Ward, Kalyan Nagar, Ring Road, near Lions School Gadag Karnataka IN 582103","c":"IN","o":"Renukas Castle Hard- and Software"}]);
---
+++ data/js/8c/1f/64/f4/e0/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f64f4e000/36",[{"d":"2022-02-06","t":"add","s":"ieee-oui36.csv","a":"Felix-Wankel-Str. 13 Zaberfeld Baden-Württemberg DE 74374","c":"DE","o":"ADAMCZEWSKI elektronische Messtechnik GmbH"}]);
