a382750f8b8159072682dd48a2068724ee0ab8fc
datagrinder <data@hdm.io>
2022-02-08 01:02:18 +0000

    Update 2022-02-08

---
+++ data/js/c8/12/0b/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("c8120b000000/24",[{"d":"2022-02-08","t":"add","s":"ieee-oui.csv","a":"129, Samsung-ro, Youngtongl-Gu Suwon Gyeonggi-Do KR 16677","c":"KR","o":"Samsung Electronics Co.,Ltd"}]);
