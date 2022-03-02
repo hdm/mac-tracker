6b80bf1553396d720741e6f6b62ec35938e9e767
datagrinder <data@hdm.io>
2022-03-02 01:02:00 +0000

    Update 2022-03-02

---
+++ data/js/78/03/4f/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("78034f000000/24",[{"d":"2022-03-02","t":"add","s":"ieee-oui.csv","a":"600 March Road Kanata Ontario CA K2K 2E6","c":"CA","o":"Nokia"}]);
---
+++ data/js/8c/1f/64/ac/00/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f64ac0000/36",[{"d":"2022-03-02","t":"add","s":"ieee-oui36.csv","a":"143B São Paulo São Paulo BR 02433-070","c":"BR","o":"AIQuatro"}]);
