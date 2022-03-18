cd1c8b2544a9c094315d75866f8e9852e0b35d6b
datagrinder <data@hdm.io>
2022-03-18 01:02:09 +0000

    Update 2022-03-18

---
+++ data/js/48/9b/e0/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("489be0000000/24",[{"d":"2022-03-18","t":"add","s":"ieee-oui.csv","a":"No.178 Yulong Avenue, Yufengshan, Yubei District, Chongqing. Chongqing  China CN 401120","c":"CN","o":"Realme Chongqing Mobile Telecommunications Corp.,Ltd."}]);
---
+++ data/js/88/f2/bd/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("88f2bd000000/24",[{"d":"2022-03-18","t":"add","s":"ieee-oui.csv","a":"Midea Global Innovation Center,Beijiao Town,Shunde Foshan Guangdong CN 528311","c":"CN","o":"GD Midea Air-Conditioning Equipment Co.,Ltd."}]);
