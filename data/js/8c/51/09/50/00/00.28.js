90e6fc2e2fffa2ef463a7e9378df24bd3cf1cf88
datagrinder <data@hdm.io>
2022-04-09 01:02:27 +0000

    Update 2022-04-09

---
+++ data/js/8c/51/09/50/00/00.28.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c5109500000/28",[{"d":"2022-04-09","t":"add","s":"ieee-mam.csv","a":"De Waal 24 Best  NL 5684 PH","c":"PH","o":"Heliox Automotive B.V."}]);
---
+++ data/js/8c/51/09/a0/00/00.28.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c5109a00000/28",[{"d":"2022-04-09","t":"add","s":"ieee-mam.csv","a":"NO.8 Tangzhuang Road,Suzhou Industrial Park,Su ZhouCity,JiangSu Province,China Suzhou  CN 215021","c":"CN","o":"SERNET (SUZHOU) TECHNOLOGIES CORPORATION"}]);
