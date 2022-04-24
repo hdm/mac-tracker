266561f583dd31bb3ecf578cde23780d3796dc6f
datagrinder <data@hdm.io>
2022-04-24 01:01:54 +0000

    Update 2022-04-24

---
+++ data/js/98/a2/c0/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("98a2c0000000/24",[{"d":"2022-04-24","t":"add","s":"ieee-oui.csv","a":"80 West Tasman Drive San Jose CA US 94568","c":"US","o":"Cisco Systems, Inc"}]);
---
+++ data/js/fc/61/79/90/00/00.28.js
@@ -0,0 +1,1 @@
+macDetailCallback("fc6179900000/28",[{"d":"2022-04-24","t":"add","s":"ieee-mam.csv","a":"Pocernicka 272/96 Prague  CZ 10800","c":"CZ","o":"MACH SYSTEMS s.r.o."}]);
