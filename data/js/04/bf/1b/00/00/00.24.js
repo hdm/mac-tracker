cd73a86d9b88c5a7aa7ee50ec96e33b764e99cf2
datagrinder <data@hdm.io>
2022-05-07 01:02:06 +0000

    Update 2022-05-07

---
+++ data/js/04/bf/1b/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("04bf1b000000/24",[{"d":"2022-05-07","t":"add","s":"ieee-oui.csv","a":"One Dell Way Round Rock TX US 78682","c":"US","o":"Dell Inc."}]);
---
+++ data/js/c8/84/8c/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("c8848c000000/24",[{"d":"2022-05-07","t":"add","s":"ieee-oui.csv","a":"350 West Java Drive Sunnyvale CA US 94089","c":"US","o":"Ruckus Wireless"}]);
