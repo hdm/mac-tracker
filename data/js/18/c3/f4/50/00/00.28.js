5c6b39e1ef571047839d7a43725589cc392dee0b
datagrinder <data@hdm.io>
2022-05-28 01:02:28 +0000

    Update 2022-05-28

---
+++ data/js/18/c3/f4/50/00/00.28.js
@@ -0,0 +1,1 @@
+macDetailCallback("18c3f4500000/28",[{"d":"2022-05-28","t":"add","s":"ieee-mam.csv","a":"1251 McKay Drive San Jose CA US  95131-1709","c":"US","o":"Synaptics, Inc"}]);
---
+++ data/js/8c/1f/64/fc/20/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f64fc2000/36",[{"d":"2022-05-28","t":"add","s":"ieee-oui36.csv","a":"1357 W Foothill Blvd Azusa CA US 91702","c":"US","o":"I/O Controls"}]);
