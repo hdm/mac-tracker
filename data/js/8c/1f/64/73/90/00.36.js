31d8b7ce6cab4add9831a10f9e48cebf8c5585e6
datagrinder <data@hdm.io>
2022-06-18 01:02:25 +0000

    Update 2022-06-18

---
+++ data/js/8c/1f/64/73/90/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f64739000/36",[{"d":"2022-06-18","t":"add","s":"ieee-oui36.csv","a":"450 South Simmons STE 670 Kaysville UT US 84037","c":"US","o":"Monnit Corporation"}]);
---
+++ data/js/8c/1f/64/d2/a0/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f64d2a000/36",[{"d":"2022-06-18","t":"add","s":"ieee-oui36.csv","a":"Veres Péter út 48 Budapest  HU 1163","c":"HU","o":"Anteus Kft."}]);
