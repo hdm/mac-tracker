b7ea45fe9d9128c1741c59271022d0a7f53e11e7
datagrinder <data@hdm.io>
2022-06-22 01:02:05 +0000

    Update 2022-06-22

---
+++ data/js/6c/65/67/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("6c6567000000/24",[{"d":"2022-06-22","t":"add","s":"ieee-oui.csv","a":"brunnenbachstrasse 1 Hinwil Zurich CH 8340","c":"CH","o":"BELIMO Automation AG"}]);
---
+++ data/js/8c/1f/64/1b/50/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f641b5000/36",[{"d":"2022-06-22","t":"add","s":"ieee-oui36.csv","a":"102 Cooper Court LOS GATOS CA US 95032","c":"US","o":"Xicato"}]);
