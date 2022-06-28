c5419c21f5a018531849b63c1f23d6fe8d3f7910
datagrinder <data@hdm.io>
2022-06-28 01:02:10 +0000

    Update 2022-06-28

---
+++ data/js/68/b8/bb/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("68b8bb000000/24",[{"d":"2022-06-28","t":"add","s":"ieee-oui.csv","a":"Xiaomi Campus Beijing Beijing CN 100085","c":"CN","o":"Beijing Xiaomi Electronics Co.,Ltd"}]);
---
+++ data/js/80/27/6c/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("80276c000000/24",[{"d":"2022-06-28","t":"add","s":"ieee-oui.csv","a":"80 West Tasman Drive San Jose CA US 94568","c":"US","o":"Cisco Systems, Inc"}]);
