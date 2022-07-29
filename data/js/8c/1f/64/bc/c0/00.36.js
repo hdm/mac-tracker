1c56cd5741b7cdedc217d1cf6f2763982e1def24
datagrinder <data@hdm.io>
2022-07-29 01:02:14 +0000

    Update 2022-07-29

---
+++ data/js/8c/1f/64/bc/c0/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f64bcc000/36",[{"d":"2022-07-29","t":"add","s":"ieee-oui36.csv","a":"650B Fremont Ave #65 Los Altos CA US 94024","c":"US","o":"Sound Health Systems"}]);
---
+++ data/js/c4/5a/b1/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("c45ab1000000/24",[{"d":"2022-07-29","t":"add","s":"ieee-oui.csv","a":"One Dell Way Round Rock TX US 78682","c":"US","o":"Dell Inc."}]);
