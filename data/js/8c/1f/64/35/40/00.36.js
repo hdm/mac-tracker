486bf243733b85d5d317ee3051c67179bbc011d5
datagrinder <data@hdm.io>
2022-08-05 01:02:10 +0000

    Update 2022-08-05

---
+++ data/js/8c/1f/64/35/40/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f64354000/36",[{"d":"2022-08-05","t":"add","s":"ieee-oui36.csv","a":"PO Box 15335 Washington DC US 20003","c":"US","o":"Paul Tagliamonte"}]);
---
+++ data/js/9c/de/4d/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("9cde4d000000/24",[{"d":"2022-08-05","t":"add","s":"ieee-oui.csv","a":"#503, 403-dong, Bucheon Techno Park, 655, Pyeongcheon-ro Bucheon-si Gyeonggi-do KR 14502","c":"KR","o":"ML vision Co.,LTD"}]);
