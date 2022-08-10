95d724eed3bb7a37444c0ed59ba379d77076ab4c
datagrinder <data@hdm.io>
2022-08-10 01:02:08 +0000

    Update 2022-08-10

---
+++ data/js/b0/8b/be/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("b08bbe000000/24",[{"d":"2022-08-10","t":"add","s":"ieee-oui.csv","a":"Albert-Buettner-Str. 11 Lauf  DE 91207","c":"DE","o":"ABL GmbH"}]);
---
+++ data/js/c4/eb/42/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("c4eb42000000/24",[{"d":"2022-08-10","t":"add","s":"ieee-oui.csv","a":"250, route de l'Empereur Rueil Malmaison Cedex hauts de seine FR 92848","c":"FR","o":"Sagemcom Broadband SAS"}]);
