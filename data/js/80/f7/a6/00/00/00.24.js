2b4ff1ed7f55efb0bacf4deb89428b4d89d8c931
datagrinder <data@hdm.io>
2022-08-30 01:02:11 +0000

    Update 2022-08-30

---
+++ data/js/80/f7/a6/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("80f7a6000000/24",[{"d":"2022-08-30","t":"add","s":"ieee-oui.csv","a":"#601, Fl6, Bldg F, No.1008  Yangguang Community, Xili, Nanshan Shenzhen Guangdong CN 518055","c":"CN","o":"Shenzhen C-Data Technology Co., Ltd."}]);
