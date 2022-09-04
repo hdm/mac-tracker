17dd7b130497e0658d08bb12f5417eccb35b00e7
datagrinder <data@hdm.io>
2022-09-04 01:02:11 +0000

    Update 2022-09-04

---
+++ data/js/80/ab/4d/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("80ab4d000000/24",[{"d":"2022-09-04","t":"add","s":"ieee-oui.csv","a":"Werinherstrasse 91 München Bavaria DE D-81541","c":"DE","o":"Nokia Solutions and Networks GmbH & Co. KG"}]);
--- data/js/e8/26/b6/00/00/00.24.js
+++ data/js/e8/26/b6/00/00/00.24.js
@@ -1,1 +1,1 @@
-macDetailCallback("e826b6000000/24",[{"d":"2019-10-18","t":"add","s":"ieee-oui.csv","a":"Strathpeffer Road Dingwall Scotland GB IV159QF","c":"GB","o":"Inside Biometrics International Limited"}]);
+macDetailCallback("e826b6000000/24",[{"d":"2019-10-18","t":"add","s":"ieee-oui.csv","a":"Strathpeffer Road Dingwall Scotland GB IV159QF","c":"GB","o":"Inside Biometrics International Limited"},{"d":"2022-09-04","t":"change","s":"ieee-oui.csv","a":"Strathpeffer Road Dingwall Scotland GB IV159QF","c":"GB","o":"Companies House to GlucoRx Technologies Ltd."}]);
