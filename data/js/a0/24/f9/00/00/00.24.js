7157ac6de01daf40c46196a050a3445e386c8a10
datagrinder <data@hdm.io>
2022-09-14 01:02:10 +0000

    Update 2022-09-14

---
+++ data/js/a0/24/f9/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("a024f9000000/24",[{"d":"2022-09-14","t":"add","s":"ieee-oui.csv","a":"High Tech District Chengdu Sichuan CN 610000","c":"CN","o":"Chengdu InnovaTest Technology Co., Ltd"}]);
---
+++ data/js/e0/38/2d/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("e0382d000000/24",[{"d":"2022-09-14","t":"add","s":"ieee-oui.csv","a":"445 Hoes Lane Piscataway NJ US 08554","c":"US","o":"IEEE Registration Authority"}]);
