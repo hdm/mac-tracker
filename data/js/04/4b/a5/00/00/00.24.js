6b2f7374dac511a7f1f1bf840a3460e414b091c8
datagrinder <data@hdm.io>
2022-10-01 01:02:22 +0000

    Update 2022-10-01

---
+++ data/js/04/4b/a5/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("044ba5000000/24",[{"d":"2022-10-01","t":"add","s":"ieee-oui.csv","a":"3/F, Building R1-B, High-Tech Industrial Park, Nanshan District Shenzhen Guangdong CN 518057","c":"CN","o":"SHENZHEN MERCURY COMMUNICATION TECHNOLOGIES CO.,LTD."}]);
---
+++ data/js/f4/4d/5c/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("f44d5c000000/24",[{"d":"2022-10-01","t":"add","s":"ieee-oui.csv","a":"No. 6 Innovation Road II, Science Park Hsichu Taiwan TW 300","c":"TW","o":"Zyxel Communications Corporation"}]);
