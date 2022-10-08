59338c0a399ffbdf68b58dfc30ec22324b00902a
datagrinder <data@hdm.io>
2022-10-08 01:02:21 +0000

    Update 2022-10-08

---
+++ data/js/84/b3/86/a0/00/00.28.js
@@ -0,0 +1,1 @@
+macDetailCallback("84b386a00000/28",[{"d":"2022-10-08","t":"add","s":"ieee-mam.csv","a":"2613 Vista Drive Huntsville AL US 35803","c":"US","o":"Velocio Networks, Inc."}]);
---
+++ data/js/a8/ee/6d/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("a8ee6d000000/24",[{"d":"2022-10-08","t":"add","s":"ieee-oui.csv","a":"13800 Coppermine Rd Herndon  VA US 20171","c":"US","o":"Fine Point-High Export"}]);
