e6f21c3aeaaebb6dc80c16732d8ddd86b67898f9
datagrinder <data@hdm.io>
2022-10-15 01:02:15 +0000

    Update 2022-10-15

---
+++ data/js/8c/1f/64/76/40/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f64764000/36",[{"d":"2022-10-15","t":"add","s":"ieee-oui36.csv","a":"1, Wood Road Wilmington DE US  19806","c":"US","o":"nanoTRONIX Computing Inc."}]);
---
+++ data/js/8c/1f/64/9a/b0/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f649ab000/36",[{"d":"2022-10-15","t":"add","s":"ieee-oui36.csv","a":"VIA TALPONEDO 29/A PORCIA PORDENONE IT 330850","c":"IT","o":"DAVE SRL"}]);
