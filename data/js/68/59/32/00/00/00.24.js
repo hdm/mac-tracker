2a20906d15c1f2edf1bb12b4c7cb120d465c54a4
datagrinder <data@hdm.io>
2022-11-24 01:02:35 +0000

    Update 2022-11-24

---
+++ data/js/68/59/32/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("685932000000/24",[{"d":"2022-11-24","t":"add","s":"ieee-oui.csv","a":"3F.,No.98-1,Mincyuan Rd.Sindian City Taipei County 231  CN 231141","c":"CN","o":"Sunitec Enterprise Co.,Ltd"}]);
---
+++ data/js/c0/ea/c3/40/00/00.28.js
@@ -0,0 +1,1 @@
+macDetailCallback("c0eac3400000/28",[{"d":"2022-11-24","t":"add","s":"ieee-mam.csv","a":"Santiniho 20/26 Ž?ár nad Sázavou  CZ 591 02","c":"CZ","o":"Tokoz a.s."}]);
