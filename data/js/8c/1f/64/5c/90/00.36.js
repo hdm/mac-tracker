eda2b915139a6e087721521945ab21a6a29bb869
datagrinder <data@hdm.io>
2022-12-20 01:02:23 +0000

    Update 2022-12-20

---
+++ data/js/8c/1f/64/5c/90/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f645c9000/36",[{"d":"2022-12-20","t":"add","s":"ieee-oui36.csv","a":"P. O.  Box 6863 Rodeløkka Oslo Oslo NO 0504","c":"NO","o":"Abbott Diagnostics Technologies AS"}]);
---
+++ data/js/8c/1f/64/f7/90/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f64f79000/36",[{"d":"2022-12-20","t":"add","s":"ieee-oui36.csv","a":"1-4-30 MEISHINGUCHI,TOYONAKA OSAKA JP 561-0841","c":"JP","o":"YUYAMA MFG Co.,Ltd"}]);
