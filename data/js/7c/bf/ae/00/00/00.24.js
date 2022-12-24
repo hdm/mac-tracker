048edf9f173a20012156a8da2e167bfe94d3f803
datagrinder <data@hdm.io>
2022-12-24 01:02:20 +0000

    Update 2022-12-24

---
+++ data/js/7c/bf/ae/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("7cbfae000000/24",[{"d":"2022-12-24","t":"add","s":"ieee-oui.csv","a":"Phase 3, Bayan Lepas FIZ Bayan Lepas Penang MY 11900","c":"MY","o":"Renesas Electronics (Penang) Sdn. Bhd."}]);
---
+++ data/js/cc/ac/fe/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("ccacfe000000/24",[{"d":"2022-12-24","t":"add","s":"ieee-oui.csv","a":"No. 1500 Zuchongzhi Rd, Building #3 Shanghai  CN 201203","c":"CN","o":"Telink Semiconductor (Shanghai) Co., Ltd."}]);
