f7a047bf5085c623859676e2c8bd2cab0cb93669
datagrinder <data@hdm.io>
2023-01-05 01:02:18 +0000

    Update 2023-01-05

---
+++ data/js/d0/16/f0/10/00/00.28.js
@@ -0,0 +1,1 @@
+macDetailCallback("d016f0100000/28",[{"d":"2023-01-05","t":"add","s":"ieee-mam.csv","a":"1003 Palladium Tower JLT Dubai, UAE Dubai  AE 393518","c":"AE","o":"QBIC COMMUNICATIONS DMCC"}]);
---
+++ data/js/d0/16/f0/c0/00/00.28.js
@@ -0,0 +1,1 @@
+macDetailCallback("d016f0c00000/28",[{"d":"2023-01-05","t":"add","s":"ieee-mam.csv","a":"5 Dreyersdal Road, Unit C, Conberg House Bergvliet Western Cape ZA 7945","c":"ZA","o":"Peralex Electronics (Pty) Ltd"}]);
