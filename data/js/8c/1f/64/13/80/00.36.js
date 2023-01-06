9b7360c7484b537d40b75b2908590592f2d221ca
datagrinder <data@hdm.io>
2023-01-06 01:02:20 +0000

    Update 2023-01-06

---
+++ data/js/8c/1f/64/13/80/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f64138000/36",[{"d":"2023-01-06","t":"add","s":"ieee-oui36.csv","a":"Robotnicza 68 C Wroclaw Dolnyslask PL 53-608","c":"PL","o":"Vissavi sp. z o.o."}]);
---
+++ data/js/8c/1f/64/63/60/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f64636000/36",[{"d":"2023-01-06","t":"add","s":"ieee-oui36.csv","a":"LUGAR SALTO LOBO\n RIANXO LA CORUÑA ES 15920","c":"ES","o":"Europe Trade"}]);
