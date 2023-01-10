63b25b6510a831a8762c16a63202f6761e0a461d
datagrinder <data@hdm.io>
2023-01-10 01:02:24 +0000

    Update 2023-01-10

---
+++ data/js/8c/1f/64/6c/b0/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f646cb000/36",[{"d":"2023-01-10","t":"add","s":"ieee-oui36.csv","a":"Unit 2, Birch Business Park, Whittle Lane Heywood  GB OL10 2SX","c":"GB","o":"GJD Manufacturing"}]);
