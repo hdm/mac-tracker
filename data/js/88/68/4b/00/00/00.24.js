e2d5a77da415c62fbd3a52c5f6f725007ef4bd12
datagrinder <data@hdm.io>
2023-01-21 01:02:30 +0000

    Update 2023-01-21

---
+++ data/js/88/68/4b/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("88684b000000/24",[{"d":"2023-01-21","t":"add","s":"ieee-oui.csv","a":"NO.18 HAIBIN ROAD, DONG GUAN GUANG DONG CN 523860","c":"CN","o":"GUANGDONG OPPO MOBILE TELECOMMUNICATIONS CORP.,LTD"}]);
---
+++ data/js/8c/1f/64/28/90/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f64289000/36",[{"d":"2023-01-21","t":"add","s":"ieee-oui36.csv","a":"Sonnenwirtleweg 9 Vaihingen Baden-Württemberg DE 71665","c":"DE","o":"Craft4 Digital GmbH"}]);
