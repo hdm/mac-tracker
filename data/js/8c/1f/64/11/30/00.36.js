b007f491b48a208596f399e2201804c32a0f3d4f
datagrinder <data@hdm.io>
2023-01-22 01:02:21 +0000

    Update 2023-01-22

---
+++ data/js/8c/1f/64/11/30/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f64113000/36",[{"d":"2023-01-22","t":"add","s":"ieee-oui36.csv","a":"1029 Blairs Ferry Rd Marion IA US 52302","c":"US","o":"Timberline Manufacturing"}]);
---
+++ data/js/8c/1f/64/c8/50/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f64c85000/36",[{"d":"2023-01-22","t":"add","s":"ieee-oui36.csv","a":"1609 Park 370 Place Hazelwood MO US 63042","c":"US","o":"Potter Electric Signal Co. LLC"}]);
