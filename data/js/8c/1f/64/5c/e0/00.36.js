396a04f444f64439926332cfcdee62a600ec6d6f
datagrinder <data@hdm.io>
2023-01-31 01:02:35 +0000

    Update 2023-01-31

---
+++ data/js/8c/1f/64/5c/e0/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f645ce000/36",[{"d":"2023-01-31","t":"add","s":"ieee-oui36.csv","a":"163 Stuyvesant Avenue Lyndhurst NJ US 07071","c":"US","o":"Packetalk LLC"}]);
---
+++ data/js/8c/1f/64/99/e0/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f6499e000/36",[{"d":"2023-01-31","t":"add","s":"ieee-oui36.csv","a":"via dell'Industria, 11 Z.I. Fontaneto  CHIERI  Turin IT 10023","c":"IT","o":"EIDOS s.r.l."}]);
