00c0989cb5adf5dc4bb7aabd95aa638d468f38f8
datagrinder <data@hdm.io>
2023-02-26 01:04:01 +0000

    Update 2023-02-26

---
+++ data/js/6c/2a/df/20/00/00.28.js
@@ -0,0 +1,1 @@
+macDetailCallback("6c2adf200000/28",[{"d":"2023-02-26","t":"add","s":"ieee-mam.csv","a":"3-2-7 Koraibashi Chuo-ku Osaka-shi Osaka-fu JP 541-0043","c":"JP","o":"DAIKO ELECTRIC CO.,LTD"}]);
---
+++ data/js/6c/2a/df/60/00/00.28.js
@@ -0,0 +1,1 @@
+macDetailCallback("6c2adf600000/28",[{"d":"2023-02-26","t":"add","s":"ieee-mam.csv","a":"ITI Limited Mankapur Mankapur Uttar Pradesh IN 271308","c":"IN","o":"ITI Limited"}]);
