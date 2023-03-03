45cdb9c09e0bfaff2e54bfea132ec65d6efe1e25
datagrinder <data@hdm.io>
2023-03-03 01:02:55 +0000

    Update 2023-03-03

---
+++ data/js/28/ec/22/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("28ec22000000/24",[{"d":"2023-03-03","t":"add","s":"ieee-oui.csv","a":"660 3rd Street San Francisco CA US 94107","c":"US","o":"eero inc."}]);
--- data/js/6c/2a/df/e0/00/00.28.js
+++ data/js/6c/2a/df/e0/00/00.28.js
@@ -1,1 +1,1 @@
-macDetailCallback("6c2adfe00000/28",[{"d":"2023-03-01","t":"add","s":"ieee-mam.csv","a":"108 Whispering Pine Dr\n#245 Scotta Valley CA US 95066","c":"US","o":"WeatherFlow-Tempest, Inc"}]);
+macDetailCallback("6c2adfe00000/28",[{"d":"2023-03-01","t":"add","s":"ieee-mam.csv","a":"108 Whispering Pine Dr\n#245 Scotta Valley CA US 95066","c":"US","o":"WeatherFlow-Tempest, Inc"},{"d":"2023-03-03","t":"change","s":"ieee-mam.csv","a":"108 Whispering Pine Dr#245 Scotts Valley CA US 95066","c":"US","o":"WeatherFlow-Tempest, Inc"}]);
