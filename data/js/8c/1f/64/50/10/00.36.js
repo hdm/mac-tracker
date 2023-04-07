21fc7d830203bf711c462736d9735c53befc05af
datagrinder <data@hdm.io>
2023-04-07 01:03:06 +0000

    Update 2023-04-07

---
+++ data/js/8c/1f/64/50/10/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f64501000/36",[{"d":"2023-04-07","t":"add","s":"ieee-oui36.csv","a":"Lilienthalstraße 5 Puchheim  DE 82178","c":"DE","o":"QUISS GmbH"}]);
---
+++ data/js/8c/1f/64/98/c0/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f6498c000/36",[{"d":"2023-04-07","t":"add","s":"ieee-oui36.csv","a":"LD Les Combet Hauts Briatexte Select State FR 81390","c":"FR","o":"PAN Business & Consulting (ANYOS]"}]);
