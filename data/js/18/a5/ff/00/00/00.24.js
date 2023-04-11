7c2972c7b7a5596f98cdb9bf9eb58cce3f358400
datagrinder <data@hdm.io>
2023-04-11 01:02:59 +0000

    Update 2023-04-11

---
+++ data/js/18/a5/ff/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("18a5ff000000/24",[{"d":"2023-04-11","t":"add","s":"ieee-oui.csv","a":"No.8, Sec.2, Guangfu Rd. Hsinchu City Hsinchu TW 30071","c":"TW","o":"Arcadyan Corporation"}]);
---
+++ data/js/b0/c7/de/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("b0c7de000000/24",[{"d":"2023-04-11","t":"add","s":"ieee-oui.csv","a":"400 West Cesar Chavez Street Austin TX US 78701","c":"US","o":"Silicon Laboratories"}]);
