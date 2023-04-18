eaa517e9b6aa1c7a399629eae263a6f143063d04
datagrinder <data@hdm.io>
2023-04-18 01:03:03 +0000

    Update 2023-04-18

---
+++ data/js/38/31/5a/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("38315a000000/24",[{"d":"2023-04-18","t":"add","s":"ieee-oui.csv","a":"103 International Drive Peachtree City GA US 30269","c":"US","o":"Rinnai"}]);
---
+++ data/js/c8/4c/78/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("c84c78000000/24",[{"d":"2023-04-18","t":"add","s":"ieee-oui.csv","a":"12/F.,zte R&D building ,kejinan Road,Shenzhen,P.R.China shenzhen  guangdong CN 518057","c":"CN","o":"zte corporation"}]);
