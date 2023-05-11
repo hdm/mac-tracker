91f6cd0fd1d562f442d15ae925c83325ebccc80d
datagrinder <data@hdm.io>
2023-05-11 01:02:56 +0000

    Update 2023-05-11

---
+++ data/js/04/09/86/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("040986000000/24",[{"d":"2023-05-11","t":"add","s":"ieee-oui.csv","a":"No.8, Sec.2, Guangfu Rd. Hsinchu City Hsinchu TW 30071","c":"TW","o":"Arcadyan Corporation"}]);
