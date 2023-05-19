7ceafb8e6cd80014021e9c147d194b3a96b08a90
datagrinder <data@hdm.io>
2023-05-19 01:02:54 +0000

    Update 2023-05-19

---
+++ data/js/88/6c/60/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("886c60000000/24",[{"d":"2023-05-19","t":"add","s":"ieee-oui.csv","a":"#019, 9th Floor, Building 6, 33 Xi'erqi Middle Road Beijing Haidian District CN 100085","c":"CN","o":"Xiaomi Communications Co Ltd"}]);
---
+++ data/js/8c/1f/64/89/90/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f64899000/36",[{"d":"2023-05-19","t":"add","s":"ieee-oui36.csv","a":"7509 NW 36th St Miami FL US 33166","c":"US","o":"American Edge IP"}]);
