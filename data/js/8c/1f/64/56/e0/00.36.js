61175d948f6b0f0ecd967e7851a464f265fe1939
datagrinder <data@hdm.io>
2023-06-02 01:03:14 +0000

    Update 2023-06-02

---
+++ data/js/8c/1f/64/56/e0/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f6456e000/36",[{"d":"2023-06-02","t":"add","s":"ieee-oui36.csv","a":"viale Colombo 23 Trezzano sul Naviglio MI IT 20090","c":"IT","o":"Euklis srl"}]);
---
+++ data/js/fc/8c/11/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("fc8c11000000/24",[{"d":"2023-06-02","t":"add","s":"ieee-oui.csv","a":"One Microsoft Way REDMOND WA US 98052","c":"US","o":"Microsoft Corporation"}]);
