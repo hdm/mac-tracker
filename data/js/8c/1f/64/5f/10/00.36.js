aa2acf4d675ca927077d3ef90cc3f223b2d40293
datagrinder <data@hdm.io>
2023-06-07 01:03:08 +0000

    Update 2023-06-07

---
+++ data/js/8c/1f/64/5f/10/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f645f1000/36",[{"d":"2023-06-07","t":"add","s":"ieee-oui36.csv","a":"No. 710, 648 Seobusaet-gil Geumcheon-gu Seoul  KR 08504","c":"KR","o":"HD Link Co., Ltd."}]);
