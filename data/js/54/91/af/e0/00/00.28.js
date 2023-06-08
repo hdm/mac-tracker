1bf3582d8673906c6f74ee4fad22cd8054310795
datagrinder <data@hdm.io>
2023-06-08 01:03:23 +0000

    Update 2023-06-08

---
+++ data/js/54/91/af/e0/00/00.28.js
@@ -0,0 +1,1 @@
+macDetailCallback("5491afe00000/28",[{"d":"2023-06-08","t":"add","s":"ieee-mam.csv","a":"Jinggangshan Economic Development Zone Ji’an Jiangxi CN 343199","c":"CN","o":"Jiangxi Anbaichuan Electric Co.(ABC),Ltd"}]);
---
+++ data/js/e4/78/76/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("e47876000000/24",[{"d":"2023-06-08","t":"add","s":"ieee-oui.csv","a":"5453 Great America Parkway Santa Clara CA US 95054","c":"US","o":"Arista Networks"}]);
