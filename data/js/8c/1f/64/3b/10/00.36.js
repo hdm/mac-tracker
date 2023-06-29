4b249d9c35b5d954cf7bc01b5d479aa0f6555990
datagrinder <data@hdm.io>
2023-06-29 01:03:08 +0000

    Update 2023-06-29

---
+++ data/js/8c/1f/64/3b/10/00.36.js
@@ -0,0 +1,1 @@
+macDetailCallback("8c1f643b1000/36",[{"d":"2023-06-29","t":"add","s":"ieee-oui36.csv","a":"Atir Yeda 15 Kfar Saba  IL 4464312","c":"IL","o":"Panoramic Power"}]);
---
+++ data/js/c0/22/f1/c0/00/00.28.js
@@ -0,0 +1,1 @@
+macDetailCallback("c022f1c00000/28",[{"d":"2023-06-29","t":"add","s":"ieee-mam.csv","a":"1835B Kramer Lane, Suite 180 Austin TX US 78758","c":"US","o":"Accelsius LLC"}]);
