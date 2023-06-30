a760fd85326c9925f59620f6c025dfa18096b51b
datagrinder <data@hdm.io>
2023-06-30 01:03:09 +0000

    Update 2023-06-30

---
+++ data/js/c0/22/f1/40/00/00.28.js
@@ -0,0 +1,1 @@
+macDetailCallback("c022f1400000/28",[{"d":"2023-06-30","t":"add","s":"ieee-mam.csv","a":"Köpmangatan 9 Örnsköldsvik Västernorrland SE 89133","c":"SE","o":"Andritz AB"}]);
---
+++ data/js/c8/9f/0c/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("c89f0c000000/24",[{"d":"2023-06-30","t":"add","s":"ieee-oui.csv","a":"222 West Merchandise Mart Plaza Chicago IL US 60654","c":"US","o":"Motorola Mobility LLC, a Lenovo Company"}]);
