e6579938f750dce3efaf9b7a3a828f37e0f6d3ec
datagrinder <data@hdm.io>
2023-07-29 01:03:22 +0000

    Update 2023-07-29

--- data/js/1c/ca/e3/70/00/00.28.js
+++ data/js/1c/ca/e3/70/00/00.28.js
@@ -1,1 +1,1 @@
-macDetailCallback("1ccae3700000/28",[{"a":"Joachimsthaler Str. 12 Berlin Berlin DE 10719","o":"Bird Home Automation GmbH","d":"2015-10-16","t":"add","s":"ieee","c":"DE"}]);
+macDetailCallback("1ccae3700000/28",[{"a":"Joachimsthaler Str. 12 Berlin Berlin DE 10719","o":"Bird Home Automation GmbH","d":"2015-10-16","t":"add","s":"ieee","c":"DE"},{"d":"2023-07-29","t":"change","s":"ieee-mam.csv","a":"Uhlandstr. 165 Berlin Berlin DE 10719","c":"DE","o":"Bird Home Automation GmbH"}]);
