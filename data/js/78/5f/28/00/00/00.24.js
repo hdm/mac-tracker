33af6560e3202fb749e9361b72a73d44f94964ac
datagrinder <data@hdm.io>
2023-08-09 01:03:29 +0000

    Update 2023-08-09

---
+++ data/js/78/5f/28/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("785f28000000/24",[{"d":"2023-08-09","t":"add","s":"ieee-oui.csv","a":"Rue des Sors 3 Marin-Epagnier Neuchatel CH 2074","c":"CH","o":"EM Microelectronic"}]);
---
+++ data/js/f8/ac/c1/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("f8acc1000000/24",[{"d":"2023-08-09","t":"add","s":"ieee-oui.csv","a":"6F, No. 157, Xinhu 1st Rd., Neihu Dist. Taipei  TW 114758","c":"TW","o":"InnoXings Co., LTD."}]);
