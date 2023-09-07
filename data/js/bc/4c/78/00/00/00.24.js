656be3c4294d7adf2cebdf6f152fc825c2415b37
datagrinder <data@hdm.io>
2023-09-07 01:03:23 +0000

    Update 2023-09-07

---
+++ data/js/bc/4c/78/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("bc4c78000000/24",[{"d":"2023-09-07","t":"add","s":"ieee-oui.csv","a":"No.2 Xin Cheng Road, Room R6,Songshan Lake Technology Park Dongguan  CN 523808","c":"CN","o":"HUAWEI TECHNOLOGIES CO.,LTD"}]);
---
+++ data/js/d0/ad/08/00/00/00.24.js
@@ -0,0 +1,1 @@
+macDetailCallback("d0ad08000000/24",[{"d":"2023-09-07","t":"add","s":"ieee-oui.csv","a":"10300 Energy Dr Spring TX US 77389","c":"US","o":"HP Inc."}]);
